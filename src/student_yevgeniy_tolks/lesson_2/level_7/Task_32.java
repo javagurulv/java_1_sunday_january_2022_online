b2323b724ad222632687e9ba432ee6e21d13ae88
ytolks <yevgeniy.tolks@gmail.com>
2022-02-23 23:17:06 +0200

    Commit of Lesson2 homework packages complete and ready for assessment/comments/advices

--- src/student_yevgeniy_tolks/lesson_2/level_7/Task_32.java
+++ src/student_yevgeniy_tolks/lesson_2/level_7/Task_32.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Task_32 {
     public static void main(String[] args) {
-        //Creating a class Scanner using java.util.Scanner library
+        //Creating a class Scanner using java.util.Scanner library (single line comments)
         Scanner scanner = new Scanner(System.in);
         /*
         Printing the line "Enter username: "
-        on the console
+        on the console (Multiline comments)
          */
         System.out.println("Enter username: ");
 
