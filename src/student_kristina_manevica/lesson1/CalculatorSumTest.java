3df41dfb6fdc153da122bc1b265dfa68ee7578a1
Kristina Manevica <uc_tuc@inbox.lv>
2022-02-24 16:34:31 +0200

    Please check

---
+++ src/student_kristina_manevica/lesson1/CalculatorSumTest.java
@@ -0,0 +1,15 @@
+package student_kristina_manevica.lesson1;
+
+public class CalculatorSumTest {
+    public static void main(String[] args){
+        int a = 74;
+        int b = 36;
+        int c = 50;
+        int d = 3;
+        int result1 = a + b;
+        int result2 = c / d;
+        System.out.println(result1);
+        System.out.println(result2);
+      }
+}
+
