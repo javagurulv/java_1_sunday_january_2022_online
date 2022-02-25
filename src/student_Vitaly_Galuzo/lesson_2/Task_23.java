3cecb30b9f10f55ca04b85a0041399745a0027c7
2vitaly2 <vitaly1992@inbox.lv>
2022-02-25 16:17:04 +0200

    Task_23

--- src/student_Vitaly_Galuzo/lesson_2/Task_23.java
+++ src/student_Vitaly_Galuzo/lesson_2/Task_23.java
@@ -1,6 +1,6 @@
-package student_Vitaly_Galuzo.lesson_2_task_23;
+package student_Vitaly_Galuzo.lesson_2;
 
-public class AttentionPlease {
+public class Task_23 {
     public static void main(String[] args) {
         System.out.println("Code outside the method will not compile!");
     }
