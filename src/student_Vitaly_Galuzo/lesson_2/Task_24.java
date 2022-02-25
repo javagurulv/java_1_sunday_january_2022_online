88cef133e56631f94b6bbc0c49e391ec6f3f46eb
2vitaly2 <vitaly1992@inbox.lv>
2022-02-25 16:17:10 +0200

    Task_24

--- src/student_Vitaly_Galuzo/lesson_2/Task_24.java
+++ src/student_Vitaly_Galuzo/lesson_2/Task_24.java
@@ -1,6 +1,6 @@
-package student_Vitaly_Galuzo.lesson_2_task_24;
+package student_Vitaly_Galuzo.lesson_2;
 
-public class NoSleepNoFocus {
+public class Task_24 {
     public static void main(String[] args) {
         System.out.println("I wanna coffee");
         System.out.println("So bad");
