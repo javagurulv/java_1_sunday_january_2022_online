04feb3d92b3bb6c478f7344e617dbc21885cf902
DD-53 <deniss0053@gmail.com>
2022-02-26 01:34:39 +0200

    Lesson 1. Homework. Level 4. Task 10.

--- src/student_deniss_dubko/deniss_dubko_lesson_1_homework/Task_10_Text.java
+++ src/student_deniss_dubko/deniss_dubko_lesson_1_homework/Task_10_Text.java
@@ -1,6 +1,6 @@
 package student_deniss_dubko.deniss_dubko_lesson_1_homework;
 
-public class Task_10 {
+public class Task_10_Text {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
