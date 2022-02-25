d9993e8d06d5f5fb77670c8d1b61b4c544d33293
2vitaly2 <vitaly1992@inbox.lv>
2022-02-25 16:16:51 +0200

    Task_15

---
+++ src/student_Vitaly_Galuzo/lesson_2/Task_15.java
@@ -0,0 +1,12 @@
+package student_Vitaly_Galuzo.lesson_2;
+
+public class Task_15 {
+    public static void main(String[] args) {
+            System.out.print("Text"+" ");
+            System.out.print("Text");
+
+
+
+
+    }
+}
