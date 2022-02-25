53186405ccc391830039e2d59eac79b8f4c82193
Devid1322 <devidannn132@gmail.com>
2022-02-25 20:15:15 +0100

    Object training

---
+++ src/student_devids_kropacovs/guessingGame/Game_tester.java
@@ -0,0 +1,8 @@
+package student_devids_kropacovs.guessingGame;
+
+public class Game_tester {
+    public static void main(String[] args) {
+        Guessing_game game = new Guessing_game();
+        game.startGame();
+    }
+}
