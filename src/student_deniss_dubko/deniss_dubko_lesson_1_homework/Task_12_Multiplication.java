4ff1915031893ed9cca9931c0d4e02d1b18880d3
DD-53 <deniss0053@gmail.com>
2022-02-26 02:24:04 +0200

    Lesson 1. Homework. Level 4. Task 12: Multiplication.

--- src/student_deniss_dubko/deniss_dubko_lesson_1_homework/Task_12_Multiplication.java
+++ src/student_deniss_dubko/deniss_dubko_lesson_1_homework/Task_12_Multiplication.java
@@ -2,7 +2,7 @@ package student_deniss_dubko.deniss_dubko_lesson_1_homework;
 
 import java.util.Scanner;
 
-public class Task_12 {
+public class Task_12_Multiplication {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите число A = ");
