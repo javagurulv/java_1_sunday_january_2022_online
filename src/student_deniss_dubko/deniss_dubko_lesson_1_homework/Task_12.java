c4b0f3b26795e09c3337119b3933baaa57d1b7aa
DD-53 <deniss0053@gmail.com>
2022-02-26 02:22:03 +0200

    Lesson 1. Homework. Level 4. Task 12: Multiplication.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_1_homework/Task_12.java
@@ -0,0 +1,16 @@
+package student_deniss_dubko.deniss_dubko_lesson_1_homework;
+
+import java.util.Scanner;
+
+public class Task_12 {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число A = ");
+        int a = input.nextInt();
+        System.out.println("Введите число B = ");
+        int b = input.nextInt();
+        int d = a*b;
+        System.out.println();
+        System.out.println("Произведение = " +d);
+    }
+}
