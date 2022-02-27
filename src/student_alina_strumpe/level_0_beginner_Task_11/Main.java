d45a88f6336447bb02f2a714799a4d079f01d9ed
Alina Strumpe <alinastrumpe@gmail.com>
2022-02-27 14:31:39 +0200

    Task_11

---
+++ src/student_alina_strumpe/level_0_beginner_Task_11/Main.java
@@ -0,0 +1,15 @@
+package student_alina_strumpe.level_0_beginner_Task_11;
+
+public class Main {
+    public static void main(String[] args){
+
+        byte a = 74;
+        byte b = 36;
+        byte sum = (byte) (a+b);
+        System.out.println("Sum = "+ sum);
+
+
+
+    }
+
+}
