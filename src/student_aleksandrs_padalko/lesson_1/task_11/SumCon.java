e73619b9c85dfaab606c47987181d506b23c5c87
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-02-28 12:58:53 +0200

    Lesson1_Task 11!

---
+++ src/student_aleksandrs_padalko/lesson_1/task_11/SumCon.java
@@ -0,0 +1,13 @@
+package student_aleksandrs_padalko.lesson_1.task_11;
+
+
+public class SumCon {
+    public static void main(String[] args) {
+        int numberOne = 74;
+        int numberTwo = 36;
+        int result = numberOne + numberTwo;
+        System.out.println( "result=" + result);
+    }
+}
+
+
