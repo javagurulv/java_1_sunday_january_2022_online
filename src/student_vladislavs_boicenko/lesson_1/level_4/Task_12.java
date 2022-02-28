baa92391fc41f2317efb0c976f328d00cbd33a14
vboicenko <vboicenko@edu.riga.lv>
2022-02-28 16:36:18 +0200

    task

---
+++ src/student_vladislavs_boicenko/lesson_1/level_4/Task_12.java
@@ -0,0 +1,13 @@
+package student_vladislavs_boicenko.lesson_1.level_4;
+
+public class Task_12 {
+
+    public static void main(String[] args) {
+
+        int num1 = 93, num2 = 53;
+        int res = num1 * num2;
+        System.out.println("Result: " + num1 + "*" + num2 + "=" + res);
+
+    }
+}
+
