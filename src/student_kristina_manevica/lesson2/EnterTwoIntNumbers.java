70af911881a5595f056db0c5eb2078564fabfab9
Kristina Manevica <uc_tuc@inbox.lv>
2022-03-01 14:54:55 +0200

    Please check

--- src/student_kristina_manevica/lesson2/EnterTwoIntNumbers.java
+++ src/student_kristina_manevica/lesson2/EnterTwoIntNumbers.java
@@ -2,7 +2,7 @@ package student_kristina_manevica.lesson2;
 
 import java.util.Scanner;
 
-public class EnterTwoNumbers {
+public class EnterTwoIntNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
