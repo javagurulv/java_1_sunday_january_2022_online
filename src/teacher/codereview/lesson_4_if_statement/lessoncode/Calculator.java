95a240902c16e144d5797e44f9a7c1277017d2a0
vsavonins <viktors.savonins@proofit.lv>
2022-03-01 17:01:59 +0200

    lesson 4

---
+++ src/teacher/codereview/lesson_4_if_statement/lessoncode/Calculator.java
@@ -0,0 +1,10 @@
+package teacher.codereview.lesson_4_if_statement.lessoncode;
+
+class Calculator {
+
+	public int sum(int firstNumber,
+				   int secondNumber) {
+		return firstNumber + secondNumber;
+	}
+
+}
