67dda1bf8615719e85aefb3d6679195d9ce058a4
Kristina Manevica <uc_tuc@inbox.lv>
2022-03-01 13:05:44 +0200

    Please check

--- src/student_kristina_manevica/lesson2/EnterTwoNumbers.java
+++ src/student_kristina_manevica/lesson2/EnterTwoNumbers.java
@@ -2,7 +2,7 @@ package student_kristina_manevica.lesson2;
 
 import java.util.Scanner;
 
-public class AddTwoNumbers {
+public class EnterTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
@@ -13,8 +13,14 @@ public class AddTwoNumbers {
         int secondNumber = scanner.nextInt();
 
         int sum = firstNumber + secondNumber;
+        int minus = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double division = firstNumber / (double) secondNumber;
 
         System.out.println("Sum of yours number is:" + sum);
+        System.out.println("Minus of yours two numbers is:" + minus);
+        System.out.println("Multiplication of yours two numbers is:" + multiplication);
+        System.out.println("Division of yours yours numbers is:" + division);
     }
 }
 
