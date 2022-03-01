d129b58b34a464119eceb7a16ebcff55245cd886
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-01 23:27:20 +0200

    Lesson2_Task 8!

---
+++ src/student_aleksandrs_padalko/lesson_2/level_1/Task_8.java
@@ -0,0 +1,15 @@
+package student_aleksandrs_padalko.lesson_2.level_1;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your  name: ");
+        String name = scanner.next();
+        System.out.println();
+        System.out.println("Hello \n"+name+" ");
+    }
+}
+
