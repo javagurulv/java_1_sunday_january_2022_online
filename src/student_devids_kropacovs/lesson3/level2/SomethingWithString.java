98e8995d71f767c16bc2146917776a4f519e2a55
Devid1322 <devidannn132@gmail.com>
2022-03-03 15:19:58 +0100

    Sorting stuff

---
+++ src/student_devids_kropacovs/lesson3/level2/SomethingWithString.java
@@ -0,0 +1,13 @@
+package student_devids_kropacovs.lesson3.level2;
+
+class SomethingWithString {
+    public static void main(String[] args) {
+        //task 7
+        String myName = "Devid";
+        System.out.println(myName);
+        //task 8
+        String greeting = "Hi! " + myName;
+
+    }
+
+}
