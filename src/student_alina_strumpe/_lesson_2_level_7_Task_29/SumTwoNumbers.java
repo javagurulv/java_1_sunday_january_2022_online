5348effe9d775f78d7607d9999cf91b949d8ada0
Alina Strumpe <alinastrumpe@gmail.com>
2022-03-05 23:36:32 +0200

    Attention please

---
+++ src/student_alina_strumpe/_lesson_2_level_7_Task_29/SumTwoNumbers.java
@@ -0,0 +1,21 @@
+package student_alina_strumpe._lesson_2_level_7_Task_29;
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int numberOne;
+        int numberTwo;
+
+
+        numberOne = 10;
+        numberTwo = 20;
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        System.out.println("Sum = " + sum);
+    }
+}
