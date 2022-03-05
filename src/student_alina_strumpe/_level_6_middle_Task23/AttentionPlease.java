a3cb556bb2d49f049498a51da64425b1a31bb4f4
Alina Strumpe <alinastrumpe@gmail.com>
2022-03-05 22:38:40 +0200

    Code inside the method.

---
+++ src/student_alina_strumpe/_level_6_middle_Task23/AttentionPlease.java
@@ -0,0 +1,13 @@
+package student_alina_strumpe._level_6_middle_Task23;
+
+public class AttentionPlease {
+
+    public static void main(String[] args) {
+
+        System.out.println("Code outside the method will not complete!");
+
+    }
+}
+
+
+
