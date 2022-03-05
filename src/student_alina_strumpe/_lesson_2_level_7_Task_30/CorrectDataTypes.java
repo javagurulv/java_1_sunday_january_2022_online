36116efa6a836937b7855ddf37bd694e81158481
Alina Strumpe <alinastrumpe@gmail.com>
2022-03-05 23:41:13 +0200

    Attention please

---
+++ src/student_alina_strumpe/_lesson_2_level_7_Task_30/CorrectDataTypes.java
@@ -0,0 +1,17 @@
+package student_alina_strumpe._lesson_2_level_7_Task_30;
+
+public class CorrectDataTypes {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+}
