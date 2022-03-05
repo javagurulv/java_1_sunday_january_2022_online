5e506268cde7266a99a36d775fe0494bd5d5ba04
Alina Strumpe <alinastrumpe@gmail.com>
2022-03-05 22:41:52 +0200

    Done

---
+++ src/student_alina_strumpe/_level_6_middle_Task23/NoSleepNoFocus.java
@@ -0,0 +1,10 @@
+package student_alina_strumpe._level_6_middle_Task23;
+
+public class NoSleepNoFocus {
+
+
+        public static void main(String[] args) {
+            System.out.println("I want  white coffee");
+            System.out.println("So bad");
+        }
+}
