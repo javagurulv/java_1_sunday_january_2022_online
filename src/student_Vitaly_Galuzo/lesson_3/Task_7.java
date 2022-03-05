8c61c6fc83c708312ba0b9cc9ed7985e33bcf487
2vitaly2 <vitaly1992@inbox.lv>
2022-03-05 20:25:08 +0200

    Task_7

---
+++ src/student_Vitaly_Galuzo/lesson_3/Task_7.java
@@ -0,0 +1,13 @@
+package student_Vitaly_Galuzo.lesson_3;
+
+public class Task_7 {
+    public static void main(String[] args) {
+
+        String name = "Vitaly";
+
+        System.out.println(name);
+
+    }
+
+
+}
