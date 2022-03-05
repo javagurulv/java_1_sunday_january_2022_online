7e9a334f0cc69a9d4f2c7870aaf8918e4f2ba7d9
2vitaly2 <vitaly1992@inbox.lv>
2022-03-05 20:10:28 +0200

    Task_1-6

---
+++ src/student_Vitaly_Galuzo/lesson_3/RobotDemo.java
@@ -0,0 +1,19 @@
+package student_Vitaly_Galuzo.lesson_3;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+             Robot rob = new Robot();
+        rob.sayHello();
+        rob.sayYourName();
+             Robot rob1 = new Robot();
+        rob1.sayHello();
+        rob1.sayYourName();
+             Robot rob2 = new Robot();
+        rob2.sayHello();
+        rob2.sayYourName();
+
+
+    }
+}
