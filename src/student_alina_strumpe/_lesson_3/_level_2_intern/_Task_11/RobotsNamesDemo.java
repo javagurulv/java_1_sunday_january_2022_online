34fb363c79ab7d9d4df199382ec60912f64df9a7
Alina Strumpe <alinastrumpe@gmail.com>
2022-03-06 20:09:42 +0200

    Robots have known their names.

---
+++ src/student_alina_strumpe/_lesson_3/_level_2_intern/_Task_11/RobotsNamesDemo.java
@@ -0,0 +1,14 @@
+package student_alina_strumpe._lesson_3._level_2_intern._Task_11;
+
+public class RobotsNamesDemo {
+    public static void main(String[] args){
+
+        RobotsNames robotsNames1 = new RobotsNames("A2");
+        RobotsNames robotsNames2 = new RobotsNames("B2");
+        RobotsNames robotsNames3 = new RobotsNames("B2B");
+
+        robotsNames1.sayYourName();
+        robotsNames2.sayYourName();
+        robotsNames3.sayYourName();
+    }
+}
