e62af578fae060351917d6938db8d448fa5ed2f4
2vitaly2 <vitaly1992@inbox.lv>
2022-03-07 12:37:45 +0200

    Task_18

---
+++ src/student_vitaly_galuzo/Dog.java
@@ -0,0 +1,23 @@
+package student_vitaly_galuzo;
+
+public class Dog {
+
+
+    String nickName;
+
+
+    public Dog(String nickName){
+        this.nickName = nickName;
+
+
+    }
+    public void dogVoice(){
+        System.out.println("Woof Woof Woof");
+
+    }
+    public void dogNickName(){
+        System.out.println("Argo");
+    }
+
+
+}
---
+++ src/student_vitaly_galuzo/DogDemo.java
@@ -0,0 +1,20 @@
+package student_vitaly_galuzo;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Argo");
+
+
+        dog.dogNickName();
+        dog.dogNickName();
+        dog.dogNickName();
+        dog.dogVoice();
+
+
+
+    }
+
+
+}
