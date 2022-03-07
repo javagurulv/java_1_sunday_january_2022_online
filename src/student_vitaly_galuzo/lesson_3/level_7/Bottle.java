e14e44febc7e644309b4f6626d0458a447bd65d7
2vitaly2 <vitaly1992@inbox.lv>
2022-03-07 19:52:52 +0200

    Task_28

---
+++ src/student_vitaly_galuzo/lesson_3/level_7/Bottle.java
@@ -0,0 +1,47 @@
+package student_vitaly_galuzo.lesson_3.level_7;
+
+public class Bottle {
+
+
+    boolean open;
+    boolean empty;
+    String brand;
+
+
+    public Bottle(boolean open,boolean empty,String brand){
+        this.open = open;
+        this.empty = empty;
+        this.brand = brand;
+    }
+
+    public String getBrand(){
+        return this.brand;
+
+    }
+
+    public void opened(){
+        this.open = true;
+
+    }
+    public void closed(){
+        this.open = false;
+
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+
+    public void notFilled(){
+        this.empty = true;
+
+    }
+    public void filled(){
+        this.empty = false;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+}
