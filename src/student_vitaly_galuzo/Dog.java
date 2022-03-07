01c03757d83915d28572690d22368e90e94e25cd
2vitaly2 <vitaly1992@inbox.lv>
2022-03-07 13:24:51 +0200

    Task_21-22

--- src/student_vitaly_galuzo/Dog.java
+++ src/student_vitaly_galuzo/Dog.java
@@ -5,18 +5,20 @@ public class Dog {
 
     String nickName;
     int age;
+    String color;
 
 
 
-    public Dog(String nickName,int age){
+    public Dog(String nickName,int age,String color){
         this.nickName = nickName;
         this.age = age;
+        this.color = color;
 
 
 
     }
     public void dogVoice(){
-        System.out.println("Woof Woof Woof");
+        System.out.println("Woof Woof Woof "+"my color is "+ this.color);
 
     }
     public void dogNickName(){
@@ -27,10 +29,15 @@ public class Dog {
         return this.age;
     }
 
+    public int happyBirthday(){
+        this.age = this.age +1;
+        return this.age;
+    }
+    public void changeColor(String newColor) {
+        color = newColor;
+        System.out.println("My new color is "+color);
 
 
-
-
-
+    }
 
 }
--- src/student_vitaly_galuzo/DogDemo.java
+++ src/student_vitaly_galuzo/DogDemo.java
@@ -4,7 +4,7 @@ public class DogDemo {
 
     public static void main(String[] args) {
 
-        Dog dog = new Dog("Argo",3);
+        Dog dog = new Dog("Argo",3,"Black");
 
 
         dog.dogNickName();
@@ -12,10 +12,11 @@ public class DogDemo {
         dog.dogNickName();
         int dogAgeIs = dog.getAge();
         dog.dogVoice();
-
         System.out.println("Dog is "+dogAgeIs+" "+"y.o");
-
-
+        dog.dogVoice();
+        int birthDay = dog.happyBirthday();
+        System.out.println("I'm now "+birthDay+" "+"y.o"+"!");
+        dog.changeColor("Gray");
 
 
     }
