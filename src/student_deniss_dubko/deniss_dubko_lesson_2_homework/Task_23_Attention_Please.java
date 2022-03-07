84949a01611afb03f22c7fbb70ce6c4e79b90f72
DD-53 <deniss0053@gmail.com>
2022-03-08 01:24:30 +0200

    Lesson 2. Homework. Level 6. Task 23: Attention Please.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_2_homework/Task_23_Attention_Please.java
@@ -0,0 +1,14 @@
+package student_deniss_dubko.deniss_dubko_lesson_2_homework;
+
+public class Task_23_Attention_Please {
+
+        public static void main(String[] args) {
+
+            System.out.println("Code outside the method will not compile!");
+            System.out.println("Repaired!");
+
+
+    }
+
+
+}
