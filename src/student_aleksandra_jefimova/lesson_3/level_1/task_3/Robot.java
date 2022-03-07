58fb5b98ddf980b908ae3788b1ff07c99afbc11a
AleksandraJef <aleksandra.jefimova@gmail.com>
2022-03-07 16:01:47 +0200

    lesson_3_task3-task6

---
+++ src/student_aleksandra_jefimova/lesson_3/level_1/task_3/Robot.java
@@ -0,0 +1,12 @@
+package student_aleksandra_jefimova.lesson_3.level_1.task_3;
+
+class Robot {
+    private String name;
+    public Robot (String robotName) {
+        this.name = robotName;
+    }
+
+    public void sayHello () {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/student_aleksandra_jefimova/lesson_3/level_1/task_5/Robot.java
@@ -0,0 +1,11 @@
+package student_aleksandra_jefimova.lesson_3.level_1.task_5;
+
+class Robot {
+    private String name;
+    public Robot(String robotName) {
+        this.name = robotName;
+    }
+    public void sayYourName () {
+        System.out.println("My name is ROBO");
+    }
+}
