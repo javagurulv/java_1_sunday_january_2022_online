a0d3506fcb7b5787386c6dc7a8be3e7afa740ac6
AleksandraJef <aleksandra.jefimova@gmail.com>
2022-03-07 11:54:06 +0200

    lesson_3_task_1

---
+++ src/student_aleksandra_jefimova/lesson_3/level_1/task_1/RobotDemo.java
@@ -0,0 +1,10 @@
+package student_aleksandra_jefimova.lesson_3.level_1.task_1;
+
+class RobotDemo {
+    public static void main(String[] args) {
+
+   Robot robot1 = new Robot( "RobotNumberOne");
+
+    robot1.sayHello();
+}
+}
