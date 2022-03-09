5a585c1204cdea9bd8f5778bb6ff9f42346c181e
Anna Zhoydik <anka22@msn.com>
2022-03-09 20:33:23 +0200

    Lesson 4 update

--- src/student_anna_zhoydik/lesson_4/task_4/EvenOdd.java
+++ src/student_anna_zhoydik/lesson_4/task_4/EvenOdd.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class EvenOdd {
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    int number = scanner.nextInt();
+    if (number % 2 == 0) {
+      System.out.println("Number is even");
+    }else{
+      System.out.println("Number is odd");
+    }
+    }
   }
-}
+
