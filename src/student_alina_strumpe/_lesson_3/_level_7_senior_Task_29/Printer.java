5f2ad7d1ccda2d1276f6fcafca112e9b22a6b64e
Alina Strumpe <alinastrumpe@gmail.com>
2022-03-09 17:05:14 +0200

    yes

--- src/student_alina_strumpe/_lesson_3/_level_7_senior_Task_29/Printer.java
+++ src/student_alina_strumpe/_lesson_3/_level_7_senior_Task_29/Printer.java
@@ -3,14 +3,18 @@ package student_alina_strumpe._lesson_3._level_7_senior_Task_29;
 public class Printer {
 
     public boolean isON;
-    public String pName;
-    public String pColor;
-    public double pPrice;
+    public String name;
+    public String color;
+    public double price;
+
+    public Printer(String pName){
+        this.name= pName;
+    }
 
     public Printer(String pName, String pColor, double pPrice, boolean isON){
-        this.pName = pName;
-        this.pColor = pColor;
-        this.pPrice = pPrice;
+        this.name = pName;
+        this.color = pColor;
+        this.price = pPrice;
         this.isON = isON;
     }
 
@@ -20,11 +24,11 @@ public class Printer {
         System.out.println("Switched ON");
     }
 
-    public void displayName(){ System.out.println("Brother");}
+   // public void displayName(){ System.out.println("Brother");}
      public void displayEmpty() { System.out.println("Out of paper");}
        public void displayReady(){ System.out.println("Ready");}
 
-    public double getPrice(){ return this.pPrice ;}
+    public double getPrice(){ return this.price ;}
 
     public void switchOFF(){
         boolean isON = false;
--- src/student_alina_strumpe/_lesson_3/_level_7_senior_Task_29/PrinterDemo.java
+++ src/student_alina_strumpe/_lesson_3/_level_7_senior_Task_29/PrinterDemo.java
@@ -5,24 +5,28 @@ public class PrinterDemo {
     public static void main(String[] args){
 
         Printer brotherPrinter = new Printer("Brother","Black",399,false);
-        Printer brotherPrinter2 = new Printer("Syster","BWhite",399,false);
+        Printer sisterPrinter = new Printer("Sister","White",399,false);
+        Printer printer2 = new Printer("NNNNNN");
 
-        String infoPrinter = brotherPrinter.pName + ", " +brotherPrinter.pColor + ", " + brotherPrinter.pPrice;
+        String infoPrinter = brotherPrinter.name + ", " +brotherPrinter.color + ", " + brotherPrinter.price;
         System.out.println("Printer *1* model , "+"color, " + "price: "+infoPrinter);
-
         brotherPrinter.switchON();
-
         brotherPrinter.displayEmpty();
-
         brotherPrinter.switchOFF();
 
 
-        String infoPrinter2 = brotherPrinter2.pName + ", "+ brotherPrinter2.pColor+", "+ brotherPrinter2.pPrice;
+        String infoPrinter2 = sisterPrinter.name + ", "+ sisterPrinter.color+", "+ sisterPrinter.price;
         System.out.println("Printer *2*  model, "+ "color, "+ "price "+infoPrinter2);
+        sisterPrinter.switchON();
+        sisterPrinter.displayReady();
+        sisterPrinter.switchOFF();
 
-        brotherPrinter2.switchON();
-        brotherPrinter2.displayReady();
-        brotherPrinter2.switchOFF();
+
+        String infoPrinter3 = printer2.name;
+        System.out.println("Printer model: "+infoPrinter3);
+        printer2.switchOFF();
+
+
 
 
 
