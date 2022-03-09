a2c346458ffd17087d12d333e614977d2629e1b7
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-09 21:43:08 +0200

    Lesson_3. Task 7&8

---
+++ src/student_aleksandrs_padalko/lesson_3/level_2/Task_7.java
@@ -0,0 +1,9 @@
+package student_aleksandrs_padalko.lesson_3.level_2;
+
+public class Task_7 { public static void main(String[] args) {
+
+    String name = "Aleksandrs";
+    String greeting = "Hi " + name + "!";
+    System.out.println(greeting);
+}
+}
