af01c05a56152cf4720ebe0cb5f2f80390e57953
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-09 21:02:10 +0200

    Lesson_3. Task 1

--- src/student_aleksandrs_padalko/lesson_3/Robot.java
+++ src/student_aleksandrs_padalko/lesson_3/Robot.java
@@ -1,14 +1,6 @@
 package student_aleksandrs_padalko.lesson_3;
 
-//Создайте класс робот (Robot).
-//
-//Научите вашего робота "говорить" привет.
-//
-//Создайте в классе Robot метод со следующей сигнатурой:
-//
-//    public void sayHello() {
-//        // тут напишите команду распечатки на консоли строки "Hello!"
-//    }
+
 
 public class Robot {
 
--- src/student_aleksandrs_padalko/lesson_3/RobotDemo.java
+++ src/student_aleksandrs_padalko/lesson_3/RobotDemo.java
@@ -4,11 +4,22 @@ package student_aleksandrs_padalko.lesson_3;
 
 
 public class RobotDemo { public static void main(String[] args) {
+
     Robot robo = new Robot("Robo");
+    Robot robot1 = new Robot("Robot1");
+    Robot robot2 = new Robot("Robot2");
 
     robo.voice();
 
     robo.voiceYourName();
+
+
+    robot1.voice();
+    robot1.voiceYourName();
+
+
+    robot2.voice();
+    robot2.voiceYourName();
 
 
 
