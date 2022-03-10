8ddd388c6ef8d9495efa8180a04ae22bfdabc8bb
AleksandraJef <aleksandra.jefimova@gmail.com>
2022-03-10 12:29:48 +0200

    lesson_3_level5_task18

---
+++ src/student_aleksandra_jefimova/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,13 @@
+package student_aleksandra_jefimova.lesson_3.level_5.task_18;
+
+class DogDemo {
+
+    public static void main(String[] args){
+
+
+Dog dog = new Dog("Pushok");
+
+        dog.voiceYourName();
+
+    }
+}
