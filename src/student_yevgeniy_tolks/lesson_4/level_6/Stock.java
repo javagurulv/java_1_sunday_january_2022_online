c04a2ffa422d57c1cac1639cf7b96e31fc50d0e9
ytolks <yevgeniy.tolks@gmail.com>
2022-03-10 11:31:39 +0200

    lesson4.level_7 final commit

---
+++ src/student_yevgeniy_tolks/lesson_4/level_6/Stock.java
@@ -0,0 +1,42 @@
+package student_yevgeniy_tolks.lesson_4.level_6;
+
+class Stock {
+    private final String companyName;
+    int currentPrice;
+    int minPrice;
+    int maxPrice;
+
+    public Stock(String companyName, int currentPrice) {
+        this.companyName = companyName;
+        this.currentPrice = currentPrice;
+        this.minPrice=currentPrice;
+        this.maxPrice=currentPrice;
+
+    }
+
+    public int updatePrice(int price) {
+
+        if(maxPrice>currentPrice && minPrice<currentPrice){
+            this.currentPrice=price;
+        }
+               else if(price>currentPrice ) {
+                    this.maxPrice = price;
+                }
+                 else   if (price < currentPrice) {
+                        this.minPrice = price;
+                }
+                        return price;
+
+                }
+
+        public String getPriceInformation () {
+
+            String getInformation = "Company name = " + this.companyName +
+                    ", Current price = " + this.currentPrice +
+                    ", Min price = " + this.minPrice +
+                    ", Max price = " + this.maxPrice;
+            return getInformation;
+        }
+
+    }
+
