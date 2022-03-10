6a46a245d9dffa67432f01ec0b6bbfd6dcec43be
ytolks <yevgeniy.tolks@gmail.com>
2022-03-10 11:53:58 +0200

    lesson_4.level_7 update

--- src/student_yevgeniy_tolks/lesson_4/level_7/FizzBuzz.java
+++ src/student_yevgeniy_tolks/lesson_4/level_7/FizzBuzz.java
@@ -4,21 +4,17 @@ class FizzBuzz {
 
     public String detect(int number){
 
-        if (number%3 == 0 && number%5==0) {
+        if (number % 3 == 0 && number%5==0) {
         return "FizzBuzz" ;
         }
-        else if (number%3 ==0)  {
+        else if (number % 3 == 0)  {
         return  "Fizz" ;
         }
-        else if (number%5==0) {
+        else if (number % 5 == 0) {
         return "Buzz" ;
         }
         else  {
             return "" + number;
         }
     }
-
-
-
-
 }
