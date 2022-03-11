d8eb9bac45e24f1020955601196e090029001d42
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-11 23:16:29 +0200

    Lesson_3. Task 31

--- src/student_aleksandrs_padalko/lesson_3/level_7/task_31/ProductDemo.java
+++ src/student_aleksandrs_padalko/lesson_3/level_7/task_31/ProductDemo.java
@@ -1,7 +1,6 @@
 package student_aleksandrs_padalko.lesson_3.level_7.task_31;
 
-
-public class ProductDemo {public static void main(String[] args) {
+class ProductDemo {public static void main(String[] args) {
     Product shirt = new Product("Shirt", 10, 20);
     String product = shirt.getProduct();
     double price = shirt.getPrice();
