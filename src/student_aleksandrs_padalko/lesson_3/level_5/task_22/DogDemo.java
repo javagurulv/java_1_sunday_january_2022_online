9f55ab20f0e855098dbe74f869c73450428044cf
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-11 16:12:11 +0200

    Lesson_3. Task 22

---
+++ src/student_aleksandrs_padalko/lesson_3/level_5/task_22/DogDemo.java
@@ -0,0 +1,17 @@
+package student_aleksandrs_padalko.lesson_3.level_5.task_22;
+
+
+
+public class DogDemo {
+    public static void main (String[] args) { Dog sharik = new Dog("Sharik", "brown", "back" );
+
+    sharik.voice();
+    sharik.color();
+    sharik.voice();
+    sharik.changeColor();
+    }
+
+
+
+}
+
