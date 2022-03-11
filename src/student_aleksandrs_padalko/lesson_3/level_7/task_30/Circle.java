f7a2950ab43bef2c268c08ac9df0cd0cdf9ab1bd
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-11 23:39:30 +0200

    Lesson_3. Task 30

---
+++ src/student_aleksandrs_padalko/lesson_3/level_7/task_30/Circle.java
@@ -0,0 +1,13 @@
+package student_aleksandrs_padalko.lesson_3.level_7.task_30;
+
+  class Circle {
+      double radius;
+
+      Circle  (double radius){
+          this.radius=radius;
+      }
+
+      public double calculateArea () {
+          return 3.141*radius*radius;
+
+  }}
