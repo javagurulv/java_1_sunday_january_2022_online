2b21df6fbb05e2431340615630436aee0495f77e
ytolks <yevgeniy.tolks@gmail.com>
2022-03-12 23:09:30 +0200

    lesson_5 commits

---
+++ src/student_yevgeniy_tolks/lesson_5/level_3/Task_17_ForLoopDefinition2.java
@@ -0,0 +1,9 @@
+package student_yevgeniy_tolks.lesson_5.level_3;
+
+class Task_17_ForLoopDefinition2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {   //after initialization ";" excpeted
+            System.out.println(i);
+        }
+    }
+}
