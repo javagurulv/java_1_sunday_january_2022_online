372ae1671b8ad555f95131636a5452efa2ccdddb
DD-53 <deniss0053@gmail.com>
2022-03-14 01:21:35 +0200

    Corrections!

--- src/student_deniss_dubko/deniss_dubko_lesson_1_homework/level_4_junior/Task12Multiplication.java
+++ src/student_deniss_dubko/deniss_dubko_lesson_1_homework/level_4_junior/Task12Multiplication.java
@@ -2,7 +2,7 @@ package student_deniss_dubko.deniss_dubko_lesson_1_homework.level_4_junior;
 
 import java.util.Scanner;
 
-public class Task_12_Multiplication {
+public class Task12Multiplication {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number A = ");
