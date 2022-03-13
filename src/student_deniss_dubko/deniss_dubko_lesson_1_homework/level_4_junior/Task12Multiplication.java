ea64570c24d143580e5e9323ecb589ba3af71cac
DD-53 <deniss0053@gmail.com>
2022-03-14 01:28:53 +0200

    Corrections!

--- src/student_deniss_dubko/deniss_dubko_lesson_1_homework/level_4_junior/Task12Multiplication.java
+++ src/student_deniss_dubko/deniss_dubko_lesson_1_homework/level_4_junior/Task12Multiplication.java
@@ -3,14 +3,22 @@ package student_deniss_dubko.deniss_dubko_lesson_1_homework.level_4_junior;
 import java.util.Scanner;
 
 public class Task12Multiplication {
+
     public static void main (String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number A = ");
-        int a = input.nextInt();
+
+        System.out.println("Enter the number A = ");
+        int aNumber = input.nextInt();
+
         System.out.println("Enter the number B = ");
-        int b = input.nextInt();
-        int d = a*b;
+        int bNumber = input.nextInt();
+
+        int multiplication = aNumber * bNumber;
+
         System.out.println();
-        System.out.println("Multiplication = " +d);
+        System.out.println("Result of Multiplication = " + multiplication);
     }
+
+
 }
