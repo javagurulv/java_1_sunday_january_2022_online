b307e61a5bee401faefde56bff6d7ff8eddf4c4b
ytolks <yevgeniy.tolks@gmail.com>
2022-03-13 23:43:01 +0200

    lesson_5.level_5 commit

---
+++ src/student_yevgeniy_tolks/lesson_5/level_5/ArrayUtil.java
@@ -0,0 +1,9 @@
+package student_yevgeniy_tolks.lesson_5.level_5;
+
+class ArrayUtil {
+
+    public int[] createArray(int arrayLenght) {
+        return new int[arrayLenght];
+    }
+ }
+
---
+++ src/student_yevgeniy_tolks/lesson_5/level_5/ArrayUtilTest.java
@@ -0,0 +1,20 @@
+package student_yevgeniy_tolks.lesson_5.level_5;
+
+public class ArrayUtilTest {
+
+    public static void main(String[] args) {
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+    }
+
+    public void shouldCreateArray() {
+
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] array = arrayUtil.createArray(1);
+        if (array.length > 0) {
+            System.out.println("Array created - Test OK!");
+        } else {
+            System.out.println("Array is not created - Test FAILED!");
+        }
+    }
+}
