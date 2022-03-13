5fac5d247048b75f29c9661813336adb586e5888
ytolks <yevgeniy.tolks@gmail.com>
2022-03-13 22:59:09 +0200

    lesson_5.level_3.Task_29 commit

--- src/student_yevgeniy_tolks/lesson_5/level_4/Task_29.java
+++ src/student_yevgeniy_tolks/lesson_5/level_4/Task_29.java
@@ -3,19 +3,19 @@ package student_yevgeniy_tolks.lesson_5.level_4;
 import java.util.Arrays;
 
 class Task_29 {
-
     public static void main(String[] args) {
 
         int[] numbers = new int[5];
 
+        for (int j = 0; j < numbers.length; j++) {
+            numbers[j] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(numbers));
+
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int) (Math.random() * 100);
             if (numbers[i] % 2 == 0) {
-                int evenNumbers = numbers[i];
-                System.out.println("Even number from array " + evenNumbers);
+                System.out.println("Even number from created array " + numbers[i]);
             }
         }
-        System.out.print("Random numbers array " + Arrays.toString(numbers));
     }
 }
-
