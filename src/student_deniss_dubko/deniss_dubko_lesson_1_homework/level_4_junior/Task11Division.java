e38d669a1aa3fd8df359aee0406adb9f72e9e549
DD-53 <deniss0053@gmail.com>
2022-03-13 18:06:34 +0200

    Corrections!

--- src/student_deniss_dubko/deniss_dubko_lesson_1_homework/level_4_junior/Task11Division.java
+++ src/student_deniss_dubko/deniss_dubko_lesson_1_homework/level_4_junior/Task11Division.java
@@ -2,15 +2,23 @@ package student_deniss_dubko.deniss_dubko_lesson_1_homework.level_4_junior;
 
 import java.util.Scanner;
 
-public class Task_11_Division {
+public class Task11Division {
+
     public static void main (String[] args) {
+
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number A = ");
-        int a = input.nextInt();
+
+        int aNumber = input.nextInt();
         System.out.println("Enter the number B = ");
-        int b = input.nextInt();
-        int d = a/b;
+
+        int bNumber = input.nextInt();
+
+        int division = aNumber/bNumber;
+
         System.out.println();
-        System.out.println("Division = " +d);
+        System.out.println("Division = " + division);
     }
+
+
 }
