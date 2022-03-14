1be1f08795934eb2608c2d35f239c154aeb9ef23
Kristina Manevica <uc_tuc@inbox.lv>
2022-03-14 13:44:19 +0200

    Please check

---
+++ src/student_kristina_manevica/lesson2/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package student_kristina_manevica.lesson2;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
