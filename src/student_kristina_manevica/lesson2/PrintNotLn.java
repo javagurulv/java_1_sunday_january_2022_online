08589139c4a93a70237898b7fe1eee0a3287cbc9
Kristina Manevica <uc_tuc@inbox.lv>
2022-03-14 13:31:07 +0200

    Please check

---
+++ src/student_kristina_manevica/lesson2/PrintNotLn.java
@@ -0,0 +1,11 @@
+package student_kristina_manevica.lesson2;
+
+import java.util.Scanner;
+
+public class PrintNotLn {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Hi!");
+    }
+}
