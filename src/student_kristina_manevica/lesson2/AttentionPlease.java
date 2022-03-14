8f094af4e80e5c45c06418ab9c5559de327b0d8a
Kristina Manevica <uc_tuc@inbox.lv>
2022-03-14 13:43:13 +0200

    Please check

---
+++ src/student_kristina_manevica/lesson2/AttentionPlease.java
@@ -0,0 +1,7 @@
+package student_kristina_manevica.lesson2;
+
+public class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
