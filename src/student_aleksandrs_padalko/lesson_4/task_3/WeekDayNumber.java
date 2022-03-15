89c1668ce5a6c8e56b3afd481f9f8bda6b443e27
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-15 22:53:32 +0200

    Lesson_4.Task_3

---
+++ src/student_aleksandrs_padalko/lesson_4/task_3/WeekDayNumber.java
@@ -0,0 +1,22 @@
+package student_aleksandrs_padalko.lesson_4.task_3;
+
+import java.util.Scanner;
+
+class WeekDayNumber {public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Insert day of the week:");
+
+    int number = sc.nextInt();
+    if(number ==1)
+    {System.out.println(" monday");      }
+
+    if(number ==2) {System.out.println(" tuesday");
+    }
+    if(number == 3) {System.out.println(" wednesday");}
+    if(number == 4) {System.out.println(" thursday");}
+    if(number == 5) {System.out.println(  " friday");}
+    if(number == 6) {System.out.println( " saturday");}
+    if(number == 7) {System.out.println( " sunday");}
+}
+}
