5120f666b1cddc1d5e731d30fa44a32349aa747f
astafjev <Astafjev94@mail.ru>
2022-03-15 09:08:10 +0200

    homework

--- src/student_stanislav_astafjev/lesson_3/level_2/Stas.java
+++ src/student_stanislav_astafjev/lesson_3/level_2/Stas.java
@@ -1,9 +1,8 @@
 package student_stanislav_astafjev.lesson_3.level_2;
 
-public class String {
+public class Stas {
     public static void main(String[] args) {
-        java.lang.String myName = ("Stas");
+        String myName = "Stas";
         System.out.println(myName);
-
     }
 }
