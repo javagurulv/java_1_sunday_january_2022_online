ca5c388785663632ddda7e68e042ea0e2ffaa511
SergioTheMaster <99146406+SergioTheMaster@users.noreply.github.com>
2022-03-15 23:51:53 +0200

    Work, work, work

--- src/student_sergei_klunkov/lesson_3/homework/level_7/task_33/ComputerDemo.java
+++ src/student_sergei_klunkov/lesson_3/homework/level_7/task_33/ComputerDemo.java
@@ -1,6 +1,6 @@
 package student_sergei_klunkov.lesson_3.homework.level_7.task_33;
 
-public class ComputerDemo {
+class ComputerDemo {
 
     public static void main(String[] args) {
         Computer dell = new Computer("Dell", "XXX-1");
