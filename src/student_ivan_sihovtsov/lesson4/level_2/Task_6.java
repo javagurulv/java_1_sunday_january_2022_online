7e2db9cd8671c8cdf18a277574db7e925e3d4e38
Ivan Sihovtsov <kumato777@mail.ru>
2022-03-16 07:26:41 +0200

    Lesson_4

---
+++ src/student_ivan_sihovtsov/lesson4/level_2/Task_6.java
@@ -0,0 +1,29 @@
+package student_ivan_sihovtsov.lesson4.level_2;
+
+import java.util.Scanner;
+
+public class Task_6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int numberOne = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int numberTwo = scanner.nextInt();
+
+        if (numberOne > numberTwo) {
+            System.out.println("Smallest number is: " + numberTwo);
+        }
+
+        if (numberTwo > numberOne) {
+            System.out.println("Smallest number is: " + numberOne);
+        } else {
+            System.out.println("The numbers are equal " + numberTwo + " = " + numberOne);
+        }
+
+
+    }
+
+}
