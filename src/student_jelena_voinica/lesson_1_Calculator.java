da021e4ce406d845e8b0dc4d589539e91f1de788
Uguha21 <Agasem2106@icloud.com>
2022-03-16 17:57:42 +0200

    Calculator(sum.)

---
+++ src/student_jelena_voinica/lesson_1_Calculator.java
@@ -0,0 +1,18 @@
+package student_jelena_voinica;
+
+import java.util.Scanner;
+
+public class lesson_1_Calculator {
+
+        public static void main(String[]args) {
+
+                int firstNumber = 10;
+                int secondNumber = 15;
+                int sum = firstNumber + secondNumber;
+                System.out.println("Sum = " + sum);
+
+        }
+
+
+
+}
