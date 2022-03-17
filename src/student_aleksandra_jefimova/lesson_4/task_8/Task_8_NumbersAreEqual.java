04b15a5e5ecf76ebdbd4a274c8ff65c010bf932d
AleksandraJef <aleksandra.jefimova@gmail.com>
2022-03-17 16:33:47 +0200

    Lesson_4_Task_9

--- src/student_aleksandra_jefimova/lesson_4/task_8/Task_8_NumbersAreEqual.java
+++ src/student_aleksandra_jefimova/lesson_4/task_8/Task_8_NumbersAreEqual.java
@@ -1,6 +1,6 @@
 package student_aleksandra_jefimova.lesson_4.task_8;
 import java.util.Scanner;
-class Task_8 { public static void main(String[] args) {
+class Task_8_NumbersAreEqual { public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Insert number One: ");
     int numberOne = scanner.nextInt();
