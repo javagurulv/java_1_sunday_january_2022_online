2eb28d4aad51b03da505951395ce9b4f0d9cff91
2vitaly2 <vitaly1992@inbox.lv>
2022-03-17 12:17:33 +0200

    Task_22

--- src/student_vitaly_galuzo/lesson_4/level_7/FizzBuzz.java
+++ src/student_vitaly_galuzo/lesson_4/level_7/FizzBuzz.java
@@ -11,12 +11,12 @@ public class FizzBuzz {
 
     public String detect( int number) {
 
-        if (number % 3 == 0) {
-            return "Fizz";
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
         } else if (number % 5 == 0) {
             return "Buzz";
-        } else if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
         } else {
             return "" + number;
         }
