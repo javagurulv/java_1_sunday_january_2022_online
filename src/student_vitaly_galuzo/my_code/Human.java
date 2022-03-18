d411ceca8ed71a2b84045a9d16344dd415b870f8
2vitaly2 <vitaly1992@inbox.lv>
2022-03-18 11:11:13 +0200

    my code

--- src/student_vitaly_galuzo/my_code/Human.java
+++ src/student_vitaly_galuzo/my_code/Human.java
@@ -5,27 +5,44 @@ public class Human {
     String name;
     int age;
     int weight;
+    String lastname;
 
 
-
-
-    public Human(String name,int age,int weight){
+    public Human(String name, int age, int weight, String lastname) {
         this.name = name;
         this.age = age;
         this.weight = weight;
+        this.lastname = lastname;
 
     }
 
-    public String getName() {
-        return this.name;
+    public String getName(String name, String lastname) {
+        return name + lastname;
 
     }
 
-    public int getAge(){
+    public int getAge() {
         return this.age;
     }
 
+    public int getNewAge() {
+        this.age = this.age + 1;
+        return this.age;
+    }
+
+
+
+
+
     public int getWeight() {
         return this.weight;
+
+    }
+
+    public int getNewWeight() {
+        this.weight = this.weight - 5;
+        return this.weight;
+
+
     }
 }
