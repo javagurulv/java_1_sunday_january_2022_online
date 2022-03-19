db3c0a1e31e7d5f9c8ae8ecca6c954cd7a99fc89
SergioTheMaster <99146406+SergioTheMaster@users.noreply.github.com>
2022-03-19 15:34:16 +0200

    Lesson 3 is changing my mind!

---
+++ src/student_sergei_klunkov/lesson_4/level_4/Calculator.java
@@ -0,0 +1,18 @@
+package student_sergei_klunkov.lesson_4.level_4;
+
+class Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    public int difference(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+    public int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+}
