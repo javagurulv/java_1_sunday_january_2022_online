4cbdf67db1fee0a11c0a6df309c5d42dd896090c
Ivan Sihovtsov <kumato777@mail.ru>
2022-03-19 15:41:12 +0200

    Lesson_4

--- src/student_ivan_sihovtsov/lesson4/level_5/task_16/LightColor.java
+++ src/student_ivan_sihovtsov/lesson4/level_5/task_16/LightColor.java
@@ -2,18 +2,18 @@ package student_ivan_sihovtsov.lesson4.level_5.task_16;
 
 public class LightColor {
 
-    public String detect(int wave){
-        if (wave > 380 && wave <= 449) {
+    public String detect(int wavelength){
+        if (wavelength > 380 && wavelength <= 449) {
             return "Violet";
-        }else if (wave > 450 && wave <= 494) {
+        }else if (wavelength > 450 && wavelength <= 494) {
             return "Blue";
-        }else if (wave > 495 && wave <= 569) {
+        }else if (wavelength > 495 && wavelength <= 569) {
             return "Green";
-        }else if (wave > 570 && wave <= 589) {
+        }else if (wavelength > 570 && wavelength <= 589) {
             return "Yellow";
-        }else if (wave > 590 && wave <= 619) {
+        }else if (wavelength > 590 && wavelength <= 619) {
             return "Orange";
-        }else if (wave > 620 && wave <= 750) {
+        }else if (wavelength > 620 && wavelength <= 750) {
             return "Red";
         }else {
             return "Invisible";
