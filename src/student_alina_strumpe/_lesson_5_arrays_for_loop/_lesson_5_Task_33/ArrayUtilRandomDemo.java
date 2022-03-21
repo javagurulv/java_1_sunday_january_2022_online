0bdeefdc30569d61a441975d39b265e41e35bc89
AlinaStrumpe <alinastrumpe@gmail.com>
2022-03-21 12:28:21 +0200

    1

--- src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_33/ArrayUtilRandomDemo.java
+++ src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_33/ArrayUtilRandomDemo.java
@@ -3,10 +3,10 @@ package student_alina_strumpe._lesson_5_arrays_for_loop._lesson_5_Task_33;
 import java.util.Random;
 
 public class ArrayUtilRandomDemo {
-     //   Random random = new Random();
+
 
     public static void main(String[] args){
         ArrayUtilRandom arrayUtilRandom = new ArrayUtilRandom();
-        arrayUtilRandom.fillArrayRandomNumbers(new int[4]);
+        arrayUtilRandom.generateArrayRandomNumbers(new int[4]);
     }
 }
