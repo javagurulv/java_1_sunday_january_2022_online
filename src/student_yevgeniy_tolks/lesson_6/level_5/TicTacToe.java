fa0dfa275753164b2d4bd1da9f71b1fe566bdb8e
ytolks <yevgeniy.tolks@gmail.com>
2022-03-22 23:15:16 +0200

    lesson6.level5 Task23 commit

--- src/student_yevgeniy_tolks/lesson_6/level_5/TicTacToe.java
+++ src/student_yevgeniy_tolks/lesson_6/level_5/TicTacToe.java
@@ -16,5 +16,17 @@ public class TicTacToe {
         }
         return winPosForHorizontals;
 
+    } public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+        boolean winPosForVerticals = false;
+
+        if (field[0][0] == playerToCheck && field[1][0] == playerToCheck && field[2][0] == playerToCheck) {
+            winPosForVerticals = true;
+        } else if (field[0][1] == playerToCheck && field[1][1] == playerToCheck && field[2][1] == playerToCheck) {
+            winPosForVerticals = true;
+        } else if (field[0][2] == playerToCheck && field[1][2] == playerToCheck && field[2][2] == playerToCheck) {
+            winPosForVerticals = true;
+        }
+        return winPosForVerticals;
+
     }
 }
--- src/student_yevgeniy_tolks/lesson_6/level_5/TicTacToeTest.java
+++ src/student_yevgeniy_tolks/lesson_6/level_5/TicTacToeTest.java
@@ -4,6 +4,8 @@ public class TicTacToeTest {
     public static void main(String[] args) {
         TicTacToeTest ticTacToeTest = new TicTacToeTest();
         ticTacToeTest.horizontalWinTest();
+        ticTacToeTest.verticalWinTest();
+
     }
 
     public void horizontalWinTest() {
@@ -18,6 +20,20 @@ public class TicTacToeTest {
             System.out.println("Horizontal win  - TEST OK");
         } else {
             System.out.println("Horizontal win FAILED");
+        }
+
+    }public void verticalWinTest() {
+        int[][] gameArray = {
+                {0, 1, 0},
+                {1, 0, 0},
+                {0, 1, 0}};
+
+        TicTacToe ticTacToe = new TicTacToe();
+        boolean resultVerticalWin = ticTacToe.isWinPositionForVerticals(gameArray, 0);
+        if (resultVerticalWin) {
+            System.out.println("Vertical win  - TEST OK");
+        } else {
+            System.out.println("Vertical win FAILED");
         }
 
     }
