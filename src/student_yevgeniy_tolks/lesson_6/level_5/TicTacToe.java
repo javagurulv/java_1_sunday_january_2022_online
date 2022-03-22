8657e1881e8bf3204d5fbf6ec8d3996fb1f3c41a
ytolks <yevgeniy.tolks@gmail.com>
2022-03-22 23:18:15 +0200

    lesson6.level5 Task24 commit

--- src/student_yevgeniy_tolks/lesson_6/level_5/TicTacToe.java
+++ src/student_yevgeniy_tolks/lesson_6/level_5/TicTacToe.java
@@ -16,7 +16,9 @@ public class TicTacToe {
         }
         return winPosForHorizontals;
 
-    } public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+    }
+
+    public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
         boolean winPosForVerticals = false;
 
         if (field[0][0] == playerToCheck && field[1][0] == playerToCheck && field[2][0] == playerToCheck) {
@@ -28,5 +30,16 @@ public class TicTacToe {
         }
         return winPosForVerticals;
 
+    }
+
+    public boolean isWinPositionForDiagonals(int[][] field, int playerToCheck) {
+        boolean winPosForDiagonals = false;
+
+        if (field[0][0] == playerToCheck && field[1][1] == playerToCheck && field[2][2] == playerToCheck) {
+            winPosForDiagonals = true;
+        } else if (field[0][2] == playerToCheck && field[1][1] == playerToCheck && field[2][0] == playerToCheck) {
+            winPosForDiagonals = true;
+        }
+        return winPosForDiagonals;
     }
 }
