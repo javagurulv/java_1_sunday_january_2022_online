bf6706483076fb7187607c3c96ae11e92597f744
AlinaStrumpe <alinastrumpe@gmail.com>
2022-03-22 12:08:53 +0200

    2;  test is my choice

--- src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_28/RanNumAndMin.java
+++ src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_28/RanNumAndMin.java
@@ -1,26 +1,44 @@
 package student_alina_strumpe._lesson_5_arrays_for_loop._lesson_5_Task_28;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class RanNumAndMin {
 
-    public static void main(String[] args) {
-
+/*    public static void main(String[] args) {
         Random random = new Random();
-
         int[] mas = new int[5];
         for (int i = 0; i < mas.length; i++) {
             int randomNumbers = random.nextInt(100);
             mas[i] = randomNumbers;
-            System.out.println("["+i+"] = " + mas[i]);
-        }
+            System.out.println("["+i+"] = " + mas[i]);}
         int min = mas[0];
         for (int i = 0; i < mas.length; i++) {
             if (mas[i] < min) {
-                min = mas[i];
-
-            }
-        }
+                min = mas[i];}}
         System.out.println("min = " + min);
-    }
+    }*/
+
+        public int[] setArray(int arrayLength){
+                return new int [arrayLength];
+        }
+
+         public void fillArrayWithRandomValue(int[] array){
+            Random random = new Random();
+            for (int i=0; i<array.length; i++){
+                array[i] = random.nextInt(50);
+            }
+         }
+
+         public void printArray(int[] array){
+            System.out.println(Arrays.toString(array));
+         }
+
+         public int findMinValueInArray(int[] array){
+            int min = array[0];
+            for(int element : array){
+                if (element < min) min = element;
+            }
+            return min;
+         }
 }
