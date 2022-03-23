2a75356d6cacd86aeb3e18e93f11a0778120bbaa
AlinaStrumpe <alinastrumpe@gmail.com>
2022-03-23 22:24:26 +0200

    2

--- src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_32/ArrayUtil.java
+++ src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_32/ArrayUtil.java
@@ -1,18 +1,7 @@
 package student_alina_strumpe._lesson_5_arrays_for_loop._lesson_5_Task_32;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
 
  class ArrayUtil {
 
-    public void generateRandomMarks(int[] numbers){
-        Random random = new Random();
-        for(int i=0; i< numbers.length; i++){
-            int randomNumbers = random.nextInt(15);
-            numbers[i] = randomNumbers;
-            System.out.println("["+i+"] = "+numbers[i]);
-        }
-
-    }
+   public int[] createNewArray(int arrayLength){ return new int [arrayLength];}
 }
