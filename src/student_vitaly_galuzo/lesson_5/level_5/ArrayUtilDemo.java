2f74343885d95b6b8feac396d422f88ae5837335
2vitaly2 <vitaly1992@inbox.lv>
2022-03-23 22:34:59 +0200

    Lesson_5

--- src/student_vitaly_galuzo/lesson_5/level_5/ArrayUtilDemo.java
+++ src/student_vitaly_galuzo/lesson_5/level_5/ArrayUtilDemo.java
@@ -5,21 +5,17 @@ import java.util.Arrays;
 public class ArrayUtilDemo {
     public static void main(String[] args) {
         ArrayUtil arrayUtil = new ArrayUtil();
+
         int[] array = arrayUtil.createArray(5);
         System.out.println(Arrays.toString(array));
         arrayUtil.fillArrayWithRandomNumbers(array);
         System.out.println(Arrays.toString(array));
-
-
-
-
-
-
-
-
-
-
-
+        arrayUtil.printArrayToConsole(array);
+        System.out.println();
+        int maxArray = arrayUtil.findMaxNumber(array);
+        System.out.println("Max number in array = "+ maxArray);
+        int minArray = arrayUtil.findMinNumber(array);
+        System.out.println("Min number in array = "+ minArray);
 
 
     }
