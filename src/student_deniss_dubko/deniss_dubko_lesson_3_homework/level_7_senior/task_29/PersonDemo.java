1296daf13e816322ea685248d563c41c6510b3c6
DD-53 <deniss0053@gmail.com>
2022-03-24 01:51:46 +0200

    Lesson 3. Homework. Level 7. Task 29: Object (Person).

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_3_homework/level_7_senior/task_29/PersonDemo.java
@@ -0,0 +1,16 @@
+package student_deniss_dubko.deniss_dubko_lesson_3_homework.level_7_senior.task_29;
+
+class PersonDemo {
+
+    public static void main(String[] args) {
+
+        Person person = new Person("Deniss", 41, "Black");
+
+        person.sayYourName();
+        person.sayYourAge();
+        person.sayHairColor();
+        person.changeHairColor();
+    }
+
+
+}
