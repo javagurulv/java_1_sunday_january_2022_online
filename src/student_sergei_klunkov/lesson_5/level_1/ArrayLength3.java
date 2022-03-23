c78443de242780dafc6ee9950d9a9eec3745a3f2
SergioTheMaster <99146406+SergioTheMaster@users.noreply.github.com>
2022-03-23 13:28:10 +0200

    Lesson 5 is crazzyyy...

---
+++ src/student_sergei_klunkov/lesson_5/level_1/ArrayLength3.java
@@ -0,0 +1,11 @@
+package student_sergei_klunkov.lesson_5.level_1;
+
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+
+    }
+
+}
