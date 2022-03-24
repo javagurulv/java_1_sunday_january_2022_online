8956dc920234bc11933ae4876f5bce0c333aa0a9
DD-53 <deniss0053@gmail.com>
2022-03-25 01:09:06 +0200

    Lesson 3. Homework. Level 7. Task 29: Object (TV).

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_3_homework/level_7_senior/task_29/TvDemo.java
@@ -0,0 +1,15 @@
+package student_deniss_dubko.deniss_dubko_lesson_3_homework.level_7_senior.task_29;
+
+class TvDemo {
+
+    public static void main(String[] args) {
+
+        Tv tv = new Tv("LG", 20, 10);
+
+        tv.showTvBrand();
+        tv.showTvVolume();
+        tv.showTvChannel();
+    }
+
+
+}
