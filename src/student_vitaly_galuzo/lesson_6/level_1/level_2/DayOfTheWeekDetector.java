3a289560dd022f771dbfd7669368a692c18cd11c
2vitaly2 <vitaly1992@inbox.lv>
2022-03-24 10:38:59 +0200

    Study

---
+++ src/student_vitaly_galuzo/lesson_6/level_1/level_2/DayOfTheWeekDetector.java
@@ -0,0 +1,27 @@
+package student_vitaly_galuzo.lesson_6.level_1.level_2;
+
+import java.util.Scanner;
+
+public class DayOfTheWeekDetector {
+
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        return scanner.nextInt();
+    }
+
+    public String findDayOfTheWeek(int dayNumber) {
+        String dayOfWeek = "";
+        switch (dayNumber) {
+            case 1 -> dayOfWeek = ("Monday");
+            case 2 -> dayOfWeek = ("Tuesday");
+            case 3 -> dayOfWeek = ("Wednesday");
+            case 4 -> dayOfWeek = ("Thursday");
+            case 5 -> dayOfWeek = ("Friday");
+            case 6 -> dayOfWeek = ("Saturday");
+            case 7 -> dayOfWeek = ("Sunday");
+            default -> dayOfWeek = ("Not correct day");
+        }
+        return dayOfWeek;
+    }
+}
