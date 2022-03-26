0d18a2ab2dd28a27f8e649f9e59fabcdd0b2c209
Elina <elina.kucenko@gmail.com>
2022-03-27 00:49:01 +0200

    lesson 6 in progress

--- src/student_elina_kucenko/lesson6/level4/task19/ForInfiniteLoopBreak.java
+++ src/student_elina_kucenko/lesson6/level4/task19/ForInfiniteLoopBreak.java
@@ -1,6 +1,6 @@
 package student_elina_kucenko.lesson6.level4.task19;
 
-public class ForInfiniteLoopBreak {
+class ForInfiniteLoopBreak {
     public static void main(String[] args) {
         for (int i = 0; true; i++) {
             System.out.println("Running infinite for loop...");
