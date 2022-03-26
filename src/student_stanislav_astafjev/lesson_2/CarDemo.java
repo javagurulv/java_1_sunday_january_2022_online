6e30118a5f47c425b5086403e02c649e5e6dbeb6
astafjev <Astafjev94@mail.ru>
2022-03-26 19:55:00 +0200

    homework

---
+++ src/student_stanislav_astafjev/lesson_2/CarDemo.java
@@ -0,0 +1,9 @@
+package student_stanislav_astafjev.lesson_2;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.model;
+        System.out.println("car model = " + carModel);
+    }
+}
