d1f2ff907959ac452d8b172ea08d5e2721555af1
DD-53 <deniss0053@gmail.com>
2022-03-27 13:33:49 +0200

    Lesson 3. Homework. Level 7. Task 30: Circle.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_3_homework/level_7_senior/task_30/CircleTest.java
@@ -0,0 +1,40 @@
+package student_deniss_dubko.deniss_dubko_lesson_3_homework.level_7_senior.task_30;
+
+public class CircleTest {
+
+    // TO BE CONTINUED
+
+    public static void main(String[] args) {
+        CircleTest circleTest = new CircleTest();
+        circleTest.shouldShowOnConsoleRadius();
+        circleTest.shouldCalculateCircleArea();
+        circleTest.shouldShowOnConsoleArea();
+    }
+
+    public void shouldShowOnConsoleRadius() {
+        Circle circle = new Circle(10);
+        String result = circle.showOnConsoleRadiusIs();
+        if (("Radius is: " + circle.circleRadius).equals(result)) {
+            System.out.println("Test Show Radius: OK");
+        } else {
+            System.out.println("Test Show Radius: FAIL");
+        }
+    }
+
+    public void shouldCalculateCircleArea() {
+        Circle circle = new Circle(10);
+
+    }
+
+    public void shouldShowOnConsoleArea() {
+        Circle circle = new Circle(10);
+        String result = circle.calculateCircleArea();
+        if (("Area is: " + circle.calculateCircleArea()).equals(result)) {
+            System.out.println("Test Show Area: OK");
+        } else {
+            System.out.println("Test Show Area: FAIL");
+        }
+    }
+
+
+}
