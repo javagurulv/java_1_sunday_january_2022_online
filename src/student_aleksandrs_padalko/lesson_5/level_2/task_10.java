4d62df7c78bc1bece3e773e62095ea4dafda7178
Alexandrs777 <aleksandrs.padalko@gmail.com>
2022-03-28 22:18:56 +0300

    Lesson_5. Level_2. Task_12

--- src/student_aleksandrs_padalko/lesson_5/level_2/task_10.java
+++ src/student_aleksandrs_padalko/lesson_5/level_2/task_10.java
@@ -1,8 +1,8 @@
-package student_aleksandrs_padalko.lesson_5.level_2.task_1;
+package student_aleksandrs_padalko.lesson_5.level_2;
 
 import java.util.Arrays;
 
-class ArrayLength {public static void main(String[] args) {
+class task_10 {public static void main(String[] args) {
     int[] numbers = new int[3];
     numbers[0] = 1;
     numbers[1] = 2;
