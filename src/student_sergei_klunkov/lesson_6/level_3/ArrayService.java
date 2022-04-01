b591e24b2dae3267a670cee43fbcfa35a3fdb8fd
SergioTheMaster <99146406+SergioTheMaster@users.noreply.github.com>
2022-04-01 18:46:04 +0300

    Lesson 6 is crazzyyy...

--- src/student_sergei_klunkov/lesson_6/level_3/ArrayService.java
+++ src/student_sergei_klunkov/lesson_6/level_3/ArrayService.java
@@ -15,4 +15,50 @@ class ArrayService {
         System.out.println(found);
         return found;
     }
+
+    public int countOccurrences(int[] array, int numberToSearch){
+
+        int count = 0;
+        for(int numberInArray : array)
+            if(numberToSearch == numberInArray){
+                count++;
+            }
+        return count;
+    }
+
+    public int[] createArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+
+    public int[] fillArray( int[] array, int numbers) {
+        for(int i=0;i< array.length;i++){
+            array[i] = numbers;
+        }
+        return array;
+    }
+
+    public boolean replaceFirst(int[] array, int numberToReplace, int newNumber){
+        boolean replace = false;
+        for ( int i = 0; i < array.length; i++){
+
+            if(array[0] == numberToReplace){
+                array[0] = newNumber;
+                return true;
+            }
+        }
+        return replace;
+    }
+
+
+    public int replaceAll(int[] array, int numberToReplace, int newNumber){
+        int replace = 0;
+        for ( int i = 0; i < array.length; i++){
+            if(array[i] == numberToReplace){
+                array[i] = newNumber;
+                replace++;
+            }
+        }
+        return replace;
+    }
 }
+
--- src/student_sergei_klunkov/lesson_6/level_3/ArrayServiceTest.java
+++ src/student_sergei_klunkov/lesson_6/level_3/ArrayServiceTest.java
@@ -2,10 +2,20 @@ package student_sergei_klunkov.lesson_6.level_3;
 
 class ArrayServiceTest {
 
+    ArrayService arrayService = new ArrayService();
+
     public static void main(String[] args) {
         ArrayServiceTest arrayServiceTest = new ArrayServiceTest();
         arrayServiceTest.containsTest();
         arrayServiceTest.notContainsTest();
+        arrayServiceTest.countOccurrencesTestPositive();
+        arrayServiceTest.countOccurrencesTestNegative();
+        arrayServiceTest.countOccurrencesTestSecondNegative();
+        arrayServiceTest.countOccurrencesTestThirdNegative();
+        arrayServiceTest.replaceFirstTest();
+        arrayServiceTest.notReplaceFirstTest();
+        arrayServiceTest.replaceAllTest();
+        arrayServiceTest.notReplaceAllTest();
 
 
     }
@@ -15,9 +25,9 @@ class ArrayServiceTest {
         int[] testArray = {1, 2, 3, 4, 5, 6};
         boolean found = true;
         if (found == contains.contains(testArray,1)){
-            System.out.println("TEST OK!");
+            System.out.println(" Contains Test -  Passed! ");
         } else {
-            System.out.println("TEST FAIL!");
+            System.out.println(" Contains Test -  Failed! ");
         }
     }
 
@@ -26,10 +36,107 @@ class ArrayServiceTest {
         int[] testArray = {1, 2, 3, 4, 5, 6};
         boolean found = true;
         if (found == contains.contains(testArray,10)){
-            System.out.println("TEST OK!");
+            System.out.println(" Not Contains Test -  Passed!");
         } else {
-            System.out.println("TEST FAIL!");
+            System.out.println(" Not Contains Test -  Failed! ");
         }
     }
 
+    public void countOccurrencesTestPositive(){
+        ArrayServiceTest countTest = new ArrayServiceTest();
+        int[] testArray = {3, 5, 6, 3, 5, 3, 3};
+        check(4,testArray," Positive Occurrences Test ");
+    }
+
+    public void countOccurrencesTestNegative(){
+        ArrayServiceTest countTest = new ArrayServiceTest();
+        int[] testArray = {3, 5, 6, 3, 5, 3, 3};
+        check2(2,testArray," Negative Occurrences Test ");
+    }
+
+    public void countOccurrencesTestSecondNegative(){
+        ArrayServiceTest countTest = new ArrayServiceTest();
+        int[] testArray = {0, 0, 0, 0, 0, 0, 0};
+        check(0,testArray," Positive Occurrences Test ");
+    }
+
+    public void countOccurrencesTestThirdNegative(){
+        ArrayServiceTest countTest = new ArrayServiceTest();
+        int[] testArray = {0, 0, 0, 0, 0, 0, 0};
+        check2(2,testArray," Negative Occurrences Test ");
+    }
+
+    public void check(int expectedResult, int[] testArray, String testName){
+        ArrayService arrayService = new ArrayService();
+
+        if (expectedResult == arrayService.countOccurrences(testArray, 3)) {
+            System.out.println(" Test " + testName + " has passed! " );
+        } else {
+            System.out.println(" Test " + testName + " has failed! ");
+
+        }
+    }
+
+    public void check2(int expectedResult, int[] testArray, String testName){
+        ArrayService arrayService = new ArrayService();
+
+        if (expectedResult != arrayService.countOccurrences(testArray, 3)) {
+            System.out.println(" Test " + testName + " has passed! " );
+        } else {
+            System.out.println(" Test " + testName + " has failed! ");
+
+        }
+    }
+
+
+    public void replaceFirstTest(){
+
+        ArrayService arrayService = new ArrayService();
+        int[] testArray = new int [5];
+        boolean replace = true;
+        arrayService.fillArray(testArray,2);
+        boolean actualResult = arrayService.replaceFirst(testArray, 2, 3);
+        if (actualResult == replace){
+            System.out.println(" Replace First Test - PASSED! ");
+        } else {
+            System.out.println(" Replace First Test - FAILED! ");
+        }
+    }
+
+    public void notReplaceFirstTest(){
+
+        ArrayService arrayService = new ArrayService();
+        int[] testArray = new int [5];
+        boolean replace = true;
+        arrayService.fillArray(testArray, 3);
+        boolean actualResult = arrayService.replaceFirst(testArray, 1, 6);
+        if (actualResult != replace){
+            System.out.println(" Not Replace First Test - PASSED! ");
+        } else {
+            System.out.println(" Not Replace First Test - FAILED! ");
+        }
+    }
+
+    public void replaceAllTest(){
+        ArrayService arrayService = new ArrayService();
+        int[] array = {1, 2, 3, 2, 5, 2};
+        int replace = 3;
+        if(arrayService.replaceAll(array, 2, 3) == replace){
+            System.out.println(" TEST PASSED! ");
+        } else {
+            System.out.println(" TEST FAILED! ");
+        }
+    }
+
+    public void notReplaceAllTest(){
+        ArrayService arrayService = new ArrayService();
+        int[] array = {1, 2, 3, 2, 5, 2};
+        int replace = 3;
+        if(arrayService.replaceAll(array, 8, 0) == 0){
+            System.out.println(" TEST PASSED! ");
+        } else {
+            System.out.println(" TEST FAILED! ");
+        }
+    }
 }
+
