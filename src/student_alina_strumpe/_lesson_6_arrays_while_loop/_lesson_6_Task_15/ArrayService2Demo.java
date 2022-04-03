65e7865a3aed681d88c35371693d170cb2364667
Alina Strumpe <alinastrumpe@gmail.com>
2022-04-03 15:48:02 +0300

    2

--- src/student_alina_strumpe/_lesson_6_arrays_while_loop/_lesson_6_Task_15/ArrayService2Demo.java
+++ src/student_alina_strumpe/_lesson_6_arrays_while_loop/_lesson_6_Task_15/ArrayService2Demo.java
@@ -2,21 +2,22 @@ package student_alina_strumpe._lesson_6_arrays_while_loop._lesson_6_Task_15;
 
 import java.util.Arrays;
 
-public class ArrayService2Demo {
+ class ArrayService2Demo {
 
     public static void main(String[] args){
 
         ArrayService2 array1 = new ArrayService2();
         int[] myArray = array1.createArray(4);
-        int numbers = array1.fillArray(myArray,8);
-        System.out.println(Arrays.toString(myArray));
-        int replaceAll = array1.replaceALL(myArray,8,2);
-        System.out.println(Arrays.toString(myArray));
+        array1.fillArray(myArray,8);
+        int result = array1.replaceALL(myArray,8,2);
+        array1.printArray(myArray,8,2);
+
 
         ArrayService2 array2 = new ArrayService2();
-        int[] myArray2 = {1,2,1,4,1};
-        int replaceAll2 = array2.replaceALL(myArray2,1,50);
-        System.out.println(Arrays.toString(myArray2));
+         int[] myArray2 = array2.createArray(6);
+        array2.fillArray(myArray2,11);
+        int result2 = array2.replaceALL(myArray2,11,50);
+        array2.printArray(myArray2,11,50);
 
     }
 }
