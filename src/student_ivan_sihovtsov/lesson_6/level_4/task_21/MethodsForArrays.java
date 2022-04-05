d07ad9ee65adca17d8c5fc52e239f2dd683d8fa3
Ivan Sihovtsov <kumato777@mail.ru>
2022-04-05 17:30:31 +0300

    Lesson_6

--- src/student_ivan_sihovtsov/lesson_6/level_4/task_21/MethodsForArrays.java
+++ src/student_ivan_sihovtsov/lesson_6/level_4/task_21/MethodsForArrays.java
@@ -4,11 +4,21 @@ import java.util.Random;
 
 public class MethodsForArrays {
 
-    public static void OutputSumOnConsole(int sum) {
-        System.out.println("The sum of the numbers in a two-dimensional array is: " + sum);
+
+    public static int[][] creatingDoubleArray(int firstArray, int secondArray) {
+        int[][] arr = new int[firstArray][secondArray];
+        return arr;
     }
 
-    public static int calculatingTheSumOfArrays(int[][] arr) {
+    public static void fillingDoubleArrayWithRandomNumbers(Random random, int[][] arr, int bound) {
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+                arr[i][j] = random.nextInt(bound);
+            }
+        }
+    }
+
+    public static int calculatingTheSumOfDoubleArrays(int[][] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++){
             for (int j = 0; j < arr[i].length; j++){
@@ -18,18 +28,12 @@ public class MethodsForArrays {
         }
         return sum;
     }
-
-    public static void fillingArrayWithRandomNumbers(Random random, int[][] arr) {
-        for (int i = 0; i < arr.length; i++){
-            for (int j = 0; j < arr[i].length; j++){
-                arr[i][j] = random.nextInt(50);
-            }
-        }
+    public static void outputSumOnConsole(int sum) {
+        System.out.println("The sum of the numbers in a two-dimensional array is: " + sum);
     }
 
-    public static int[][] createArray(int firstArray, int secondArray) {
-        int[][] arr = new int[firstArray][secondArray];
-        return arr;
-    }
+
+
+
 
 }
