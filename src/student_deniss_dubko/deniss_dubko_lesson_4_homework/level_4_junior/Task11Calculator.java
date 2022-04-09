fbb45d8f20a14526792d3c9bddd54cb1f4a56182
DD-53 <deniss0053@gmail.com>
2022-04-10 00:49:57 +0200

    Lesson 4. Homework. Level 4. Task 11: Calculator.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_4_homework/level_4_junior/Task11Calculator.java
@@ -0,0 +1,22 @@
+package student_deniss_dubko.deniss_dubko_lesson_4_homework.level_4_junior;
+
+class Task11Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int difference(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+
+}
