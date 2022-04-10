c5ceaacbe6ab3a34fca17fbe095e56a2ec168b37
DD-53 <deniss0053@gmail.com>
2022-04-10 13:36:21 +0200

    Lesson 4. Homework. Level 4. Task 12: Calculator.

--- src/student_deniss_dubko/deniss_dubko_lesson_4_homework/level_4_junior/task_11_12/Calculator.java
+++ src/student_deniss_dubko/deniss_dubko_lesson_4_homework/level_4_junior/task_11_12/Calculator.java
@@ -1,4 +1,4 @@
-package student_deniss_dubko.deniss_dubko_lesson_4_homework.level_4_junior.task_11;
+package student_deniss_dubko.deniss_dubko_lesson_4_homework.level_4_junior.task_11_12;
 
 class Calculator {
 
