622926b31fb01778b8a7f905d1309642683ffb41
Devid1322 <devidannn132@gmail.com>
2022-04-15 15:07:12 +0200

    Palindrome test done

--- src/student_devids_kropacovs/lesson7/task11/PalindromeTest.java
+++ src/student_devids_kropacovs/lesson7/task11/PalindromeTest.java
@@ -6,11 +6,12 @@ class PalindromeTest {
     public static void main(String[] args) {
         PalindromeTest palindromeTest = new PalindromeTest();
         palindromeTest.isPalindromeTest();
+        palindromeTest.isNotPalindromeTest();
     }
 
     public void isPalindromeTest(){
         Palindrome palindrome = new Palindrome();
-        String text = "sum summus mus";
+        String text = "sum sumMuS mus";
         boolean expectedResult = true;
         boolean realResult = palindrome.isPalindrome(text);
         if(expectedResult == realResult){
@@ -20,5 +21,16 @@ class PalindromeTest {
         }
     }
 
+    public void isNotPalindromeTest(){
+        Palindrome palindrome = new Palindrome();
+        String text = "Not a palindrome test";
+        boolean expectedResult = false;
+        boolean realResult = palindrome.isPalindrome(text);
+        if(expectedResult == realResult){
+            System.out.println("Is palindrome test PASS");
+        }else{
+            System.out.println("Is palindrome test FAIL");
+        }
+    }
 
 }
