642059b2415fc5799259a4e38fb0dc468b79dcda
Ivan Sihovtsov <kumato777@mail.ru>
2022-04-15 12:08:49 +0300

    Lesson_6

---
+++ src/student_ivan_sihovtsov/lesson_6/level_3/ArrayServiceTest.java
@@ -0,0 +1,33 @@
+package student_ivan_sihovtsov.lesson_6.level_3;
+
+public class ArrayServiceTest {
+
+    public static void main(String[] args) {
+        ArrayServiceTest arrayServiceTest = new ArrayServiceTest();
+        arrayServiceTest.numberToSearchPositive();
+        arrayServiceTest.numberToSearchNegative();
+    }
+
+    public void numberToSearchPositive() {
+       ArrayService arrayService = new ArrayService();
+       boolean arr = arrayService.contains(new int[]{5, 8, 32, 336},32);
+       if (arr == true){
+           System.out.println("Positive test is OK");
+       }else {
+           System.out.println("Positive test is Fail");
+       }
+
+    }
+
+    public void numberToSearchNegative() {
+        ArrayService arrayService = new ArrayService();
+        boolean arr = arrayService.contains(new int[]{5, 8, 32, 336},31);
+        if (arr == true){
+            System.out.println("Negative test is OK");
+        }else {
+            System.out.println("Negative test is Fail");
+        }
+
+    }
+
+}
