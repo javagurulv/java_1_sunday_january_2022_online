2ee83d8566c7ffd5314cafcd3076206ad63832f6
DD-53 <deniss0053@gmail.com>
2022-04-17 00:13:50 +0200

    Lesson 5. Homework. Level 2. Task 13: Sum of three random array numbers.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_5_homework/level_2_intern/Task13SumOfThreeRandomArrayNumbers.java
@@ -0,0 +1,25 @@
+package student_deniss_dubko.deniss_dubko_lesson_5_homework.level_2_intern;
+
+import java.util.Random;
+
+class Task13SumOfThreeRandomArrayNumbers {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] randomMarks = new int[3];
+        for (int i = 0; i < randomMarks.length; i++) {
+            int randomMark = random.nextInt();
+            randomMarks[i] = randomMark;
+            System.out.println(randomMarks[i]);
+        }
+
+        int sumOfArrayNumbers = 0;
+        for( int i : randomMarks) {
+            sumOfArrayNumbers += i;
+            System.out.println("Sum of array numbers: " + sumOfArrayNumbers);
+        }
+    }
+
+}
