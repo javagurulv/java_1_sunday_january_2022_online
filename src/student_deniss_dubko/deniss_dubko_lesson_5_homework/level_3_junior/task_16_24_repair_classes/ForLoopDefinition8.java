3f7dada77a91536dc767056b4f8676f059bbb5d0
DD-53 <deniss0053@gmail.com>
2022-04-18 15:56:37 +0200

    Lesson 5. Homework. Level 3. Task 16 - 24: Repair classes.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_5_homework/level_3_junior/task_16_24_repair_classes/ForLoopDefinition8.java
@@ -0,0 +1,26 @@
+package student_deniss_dubko.deniss_dubko_lesson_5_homework.level_3_junior.task_16_24_repair_classes;
+
+/*
+Найдите логическую ошибку в программе и исправить её:
+
+class ForLoopDefinition8 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+    }
+
+}
+*/
+
+class ForLoopDefinition8 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
+    }
+
+}
