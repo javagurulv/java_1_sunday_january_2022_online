268c6a79b1e9dc57554855d158e692f5d862bd66
DD-53 <deniss0053@gmail.com>
2022-04-18 15:59:11 +0200

    Lesson 5. Homework. Level 3. Task 16 - 24: Repair classes.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_5_homework/level_3_junior/task_16_24_repair_classes/ForLoopDefinition9.java
@@ -0,0 +1,26 @@
+package student_deniss_dubko.deniss_dubko_lesson_5_homework.level_3_junior.task_16_24_repair_classes;
+
+/*
+Исправьте код таким образом, что бы программа
+выводила на консоль числа от 0 до 100:
+
+class ForLoopDefinition8 {
+
+    public static void main(String[] args) {
+        for ( ; ; ) {
+            System.out.println(i);
+        }
+    }
+
+}
+*/
+
+class ForLoopDefinition9 {
+
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 101 ; i++ ) {
+            System.out.println(i);
+        }
+    }
+
+}
