1eb398d00168005c42a28fa80eeeef3201cc72b6
DD-53 <deniss0053@gmail.com>
2022-04-18 15:29:18 +0200

    Lesson 5. Homework. Level 3. Task 16: Syntax mistake.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_5_homework/level_3_junior/task_16_syntax_mistake_1/ForLoopDefinition1.java
@@ -0,0 +1,24 @@
+package student_deniss_dubko.deniss_dubko_lesson_5_homework.level_3_junior.task_16_syntax_mistake_1;
+
+/* Найдите синтаксическую ошибку в программе и исправьте её:
+
+class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+        for (int i; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+ */
+
+class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
