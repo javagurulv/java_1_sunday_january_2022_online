90706b819413f41d48f32eaa74b00dba713ee30c
AlinaStrumpe <alinastrumpe@gmail.com>
2022-04-18 06:59:46 +0300

    2, fill an array test

--- src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_33/ArrayUtilRandomDemo.java
+++ src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_33/ArrayUtilRandomDemo.java
@@ -10,5 +10,13 @@ import java.util.Random;
         int[] myArray = arrayUtilRandom.createMyArray(5);
         arrayUtilRandom.fillArrayRandomNumbers(myArray);
         arrayUtilRandom.printArray(myArray);
+
+        int myArr[] = { 2, 5 , 10 ,35 ,100 };
+        int result = 0;
+        int i;
+        for(i = 0; i < myArr.length; i++)
+        {
+            result = result + myArr[i];
     }
-}
+        System.out.println(result);
+}}
--- src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_33/ArrayUtilRandomTest.java
+++ src/student_alina_strumpe/_lesson_5_arrays_for_loop/_lesson_5_Task_33/ArrayUtilRandomTest.java
@@ -2,19 +2,40 @@ package student_alina_strumpe._lesson_5_arrays_for_loop._lesson_5_Task_33;
 
 public class ArrayUtilRandomTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         ArrayUtilRandomTest arrayUtilRandomTest = new ArrayUtilRandomTest();
         arrayUtilRandomTest.shouldCreateArray();
+        arrayUtilRandomTest.shouldFillRandom();
+       //   shouldNOTfill array  - ????
     }
 
-    public void shouldCreateArray(){
+    public void shouldCreateArray() {
         ArrayUtilRandom arrayUtilRandom = new ArrayUtilRandom();
         int[] createdArray = arrayUtilRandom.createMyArray(2);
-        if(createdArray.length > 0){
+        if (createdArray.length > 0) {
             System.out.println("Test should create is OK");
-        } else{
+        } else {
             System.out.println("Test should create is FAILED");
         }
     }
+
+    public void shouldFillRandom() {
+        ArrayUtilRandom arrayUtilRandom = new ArrayUtilRandom();
+        int[] testArray = new int[3];
+        arrayUtilRandom.fillArrayRandomNumbers(testArray);
+        int result = 0;
+        int i;
+        for (i = 0; i < testArray.length; i++) {
+            result += testArray[i];
+        }
+        if (result > 0) {
+            System.out.println("Test should fill array is OK");
+        } else {
+            System.out.println("\"Test should NOT fill array is FAIL\"");
+        }
+    }
+
 }
+
+
