2ce2239ce35c7e9a5507db29ed8c40f8513d408c
ytolks <yevgeniy.tolks@gmail.com>
2022-04-19 15:43:20 +0300

    lesson8.level1.task2 commit

--- src/student_yevgeniy_tolks/lesson_8/level_1/Trader.java
+++ src/student_yevgeniy_tolks/lesson_8/level_1/Trader.java
@@ -2,6 +2,11 @@ package student_yevgeniy_tolks.lesson_8.level_1;
 
 class Trader {
 
-    private String fulName;
+    private String fullName;
     private String city;
+
+    public Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city = city;
+    }
 }
