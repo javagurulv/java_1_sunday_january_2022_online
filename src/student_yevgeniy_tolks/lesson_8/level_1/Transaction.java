25c187c4d760e047ebfdbc618eb7985e7e6311bd
ytolks <yevgeniy.tolks@gmail.com>
2022-04-19 15:48:34 +0300

    lesson8.level1.task4-5 commit

--- src/student_yevgeniy_tolks/lesson_8/level_1/Transaction.java
+++ src/student_yevgeniy_tolks/lesson_8/level_1/Transaction.java
@@ -4,4 +4,17 @@ class Transaction {
 
     private Trader trader;
     private int amount;
+
+    public Transaction(Trader trader, int amount) {
+        this.trader = trader;
+        this.amount = amount;
+    }
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
