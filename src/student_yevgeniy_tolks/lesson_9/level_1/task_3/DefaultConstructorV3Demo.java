fb3e0d28789a177de0f1da96a45f2631aa1aeef9
ytolks <yevgeniy.tolks@gmail.com>
2022-04-23 18:30:01 +0300

    lesson1.level1.task3 default constructor

---
+++ src/student_yevgeniy_tolks/lesson_9/level_1/task_3/DefaultConstructorV3Demo.java
@@ -0,0 +1,13 @@
+package student_yevgeniy_tolks.lesson_9.level_1.task_3;
+
+import student_yevgeniy_tolks.lesson_9.level_1.task_1.DefaultConstructor;
+
+class DefaultConstructorV3Demo {
+    public static void main(String[] args) {
+        DefaultConstructorV3 defaultConstructorV3 = new DefaultConstructorV3();
+        System.out.println(defaultConstructorV3.getFullName());
+        System.out.println(defaultConstructorV3.getAge());
+        System.out.println(defaultConstructorV3.getFemale());
+        System.out.println(defaultConstructorV3.isMale());
+    }
+}
