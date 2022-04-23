bd74a828e826eeb0c5851db700f51d4747edb35b
ytolks <yevgeniy.tolks@gmail.com>
2022-04-23 19:40:49 +0300

    lesson1.level1.task7 inheritance

---
+++ src/student_yevgeniy_tolks/lesson_9/level_2/task_7/HumanPlayer.java
@@ -0,0 +1,8 @@
+package student_yevgeniy_tolks.lesson_9.level_2.task_7;
+
+class HumanPlayer extends Player {
+
+    HumanPlayer(String name){
+        super(name);
+    }
+}
---
+++ src/student_yevgeniy_tolks/lesson_9/level_2/task_7/Player.java
@@ -0,0 +1,9 @@
+package student_yevgeniy_tolks.lesson_9.level_2.task_7;
+
+class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
