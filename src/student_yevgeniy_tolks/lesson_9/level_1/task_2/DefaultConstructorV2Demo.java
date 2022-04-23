886614b971bf509eafae0ededa14f7ba2a8153b9
ytolks <yevgeniy.tolks@gmail.com>
2022-04-23 18:17:54 +0300

    lesson1.level1.task2 default constructor implicit creation

---
+++ src/student_yevgeniy_tolks/lesson_9/level_1/task_2/DefaultConstructorV2Demo.java
@@ -0,0 +1,9 @@
+package student_yevgeniy_tolks.lesson_9.level_1.task_2;
+
+class DefaultConstructorV2Demo {
+    public static void main(String[] args) {
+        DefaultConstructorV2 defaultConstructorV2 = new DefaultConstructorV2(); //default constructor automatically
+                                                                                // implicitly created without parameter
+        System.out.println(defaultConstructorV2.fieldValue);
+    }
+}
