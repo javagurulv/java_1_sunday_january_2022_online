098121dce1be2460cc7b80c6464f63a0e0ddbe3e
astafjev <Astafjev94@mail.ru>
2022-04-24 12:10:32 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_3/Task_15/Phone.java
@@ -0,0 +1,11 @@
+package student_stanislav_astafjev.lesson_3.Task_15;
+
+public class Phone {
+    String model;
+    Phone(String newModel){
+        this.model = newModel;
+    }
+    String getModel(){
+        return this.model;
+    }
+}
