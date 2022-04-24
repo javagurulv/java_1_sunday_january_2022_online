1aa09f0222d4eff7d2c1cb07ce18f3e48cff6b7b
astafjev <Astafjev94@mail.ru>
2022-04-24 11:32:04 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_3/Task_1/Robot.java
@@ -0,0 +1,10 @@
+package student_stanislav_astafjev.lesson_3.Task_1;
+
+public class Robot {
+    public void sayHello(){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
