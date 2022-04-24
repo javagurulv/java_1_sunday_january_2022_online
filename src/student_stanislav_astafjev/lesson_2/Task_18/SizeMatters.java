fb32cd349ac1cf1cb4a78c6eb257e8c9a0a32b60
astafjev <Astafjev94@mail.ru>
2022-04-24 10:13:52 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_2/Task_18/SizeMatters.java
@@ -0,0 +1,9 @@
+package student_stanislav_astafjev.lesson_2.Task_18;
+
+
+class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
