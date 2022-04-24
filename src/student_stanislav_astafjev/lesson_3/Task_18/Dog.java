bf7f4155adb62a50925566b8e1f803afda390524
astafjev <Astafjev94@mail.ru>
2022-04-24 13:45:13 +0300

    homework

--- src/student_stanislav_astafjev/lesson_3/Task_18/Dog.java
+++ src/student_stanislav_astafjev/lesson_3/Task_18/Dog.java
@@ -3,21 +3,31 @@ package student_stanislav_astafjev.lesson_3.Task_18;
  class Dog {
      String name;
      int age;
-     Dog(String dogName, int dogAge) {
+     String dogColor;
+
+     Dog(String dogName, int dogAge,String dogColor) {
         this.name = dogName;
         this.age = dogAge;
+        this.dogColor = dogColor;
     }
      void voice() {
         System.out.println("AWWW AWWW AWWW");
     }
      void voiceYourName() {
-        System.out.println("Dog name is " + this.name);
+         System.out.println("Dog name is " + this.name);
     }
      void voiceYourAge() {
         System.out.println("Dog age is " + this.age);
     }
+    void dogColor() {
+         System.out.println("Dog color is " + this.dogColor);
+    }
     void happyBirthday(){
          this.age = this.age+1;
          System.out.println(age + " years");
+    }
+    void changeColor(String newColor) {
+         this.dogColor = newColor;
+         System.out.println("Change color to " + newColor);
     }
 }
--- src/student_stanislav_astafjev/lesson_3/Task_18/DogDemo.java
+++ src/student_stanislav_astafjev/lesson_3/Task_18/DogDemo.java
@@ -2,14 +2,16 @@ package student_stanislav_astafjev.lesson_3.Task_18;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog sharik = new Dog("Sharik",10);
+        Dog sharik = new Dog("Sharik",10,"red");
         sharik.voiceYourName();
         sharik.voice();
-
+        sharik.dogColor();
         sharik.voiceYourAge();
         System.out.println("Sharik celebrating birthday!");
         sharik.voice();
+        String dogColor = sharik.dogColor;
         sharik.happyBirthday();
+        sharik.changeColor("black");
 
     }
 }
