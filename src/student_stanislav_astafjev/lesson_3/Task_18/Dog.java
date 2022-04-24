4cfecaacc16f3d002803b6924cfa3e7736884fdc
astafjev <Astafjev94@mail.ru>
2022-04-24 12:50:26 +0300

    homework

--- src/student_stanislav_astafjev/lesson_3/Task_18/Dog.java
+++ src/student_stanislav_astafjev/lesson_3/Task_18/Dog.java
@@ -1,19 +1,23 @@
 package student_stanislav_astafjev.lesson_3.Task_18;
 
-public class Dog {
-    private String name;
-    private int age;
-    public Dog(String dogName, int dogAge) {
+ class Dog {
+     String name;
+     int age;
+     Dog(String dogName, int dogAge) {
         this.name = dogName;
         this.age = dogAge;
     }
-    public void voice() {
+     void voice() {
         System.out.println("AWWW AWWW AWWW");
     }
-    public void voiceYourName() {
+     void voiceYourName() {
         System.out.println("Dog name is " + this.name);
     }
-    public void voiceYourAge() {
+     void voiceYourAge() {
         System.out.println("Dog age is " + this.age);
+    }
+    void happyBirthday(){
+         this.age = this.age+1;
+         System.out.println(age + " years");
     }
 }
--- src/student_stanislav_astafjev/lesson_3/Task_18/DogDemo.java
+++ src/student_stanislav_astafjev/lesson_3/Task_18/DogDemo.java
@@ -3,10 +3,13 @@ package student_stanislav_astafjev.lesson_3.Task_18;
 public class DogDemo {
     public static void main(String[] args) {
         Dog sharik = new Dog("Sharik",10);
+        sharik.voiceYourName();
         sharik.voice();
-        sharik.voiceYourName();
-        sharik.voiceYourName();
-        sharik.voiceYourName();
+
         sharik.voiceYourAge();
+        System.out.println("Sharik celebrating birthday!");
+        sharik.voice();
+        sharik.happyBirthday();
+
     }
 }
