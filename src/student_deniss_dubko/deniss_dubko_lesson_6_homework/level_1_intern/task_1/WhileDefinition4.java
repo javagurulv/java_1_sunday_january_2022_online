532480b55271638fd8e27a6b98f2c21840f89cbd
DD-53 <deniss0053@gmail.com>
2022-04-24 15:37:01 +0200

    Lesson 6. Homework. Level 1. Task 4: While Definition.

--- src/student_deniss_dubko/deniss_dubko_lesson_6_homework/level_1_intern/task_1/WhileDefinition4.java
+++ src/student_deniss_dubko/deniss_dubko_lesson_6_homework/level_1_intern/task_1/WhileDefinition4.java
@@ -1,22 +1,23 @@
 package student_deniss_dubko.deniss_dubko_lesson_6_homework.level_1_intern.task_1;
 
 /*
-Найдите ошибку в программе и исправить её:
+Найдите логическую ошибку в программе и исправить её.
+Программа должна выводить числа от 1 до 100 на консоль.
 
-class WhileDefinition2 {
+class WhileDefinition4 {
 
     public static void main(String[] args) {
         int i = 1;
-        while (i <= 100)
+        while (i <= 100) {
             System.out.println(i);
-            i++;
         }
+        i++;
     }
 
 }
 */
 
-class WhileDefinition2 {
+class WhileDefinition4 {
 
     public static void main(String[] args) {
         int i = 1;
