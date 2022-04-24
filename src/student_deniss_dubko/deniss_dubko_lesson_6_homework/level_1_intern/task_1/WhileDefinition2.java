9683e3f50910069e6d8dd9a443efcc4cbdea0e37
DD-53 <deniss0053@gmail.com>
2022-04-24 15:30:53 +0200

    Lesson 6. Homework. Level 1. Task 2: While Definition.

---
+++ src/student_deniss_dubko/deniss_dubko_lesson_6_homework/level_1_intern/task_1/WhileDefinition2.java
@@ -0,0 +1,29 @@
+package student_deniss_dubko.deniss_dubko_lesson_6_homework.level_1_intern.task_1;
+
+/*
+Найдите ошибку в программе и исправить её:
+
+class WhileDefinition2 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100)
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
+*/
+
+class WhileDefinition2 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+        i++;
+        }
+    }
+
+}
