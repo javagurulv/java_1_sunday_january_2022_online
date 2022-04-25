09f19b7e457384cf2d01b5bb874c57646ed501cd
astafjev <Astafjev94@mail.ru>
2022-04-25 11:57:18 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_4/Task_3/Week.java
@@ -0,0 +1,36 @@
+package student_stanislav_astafjev.lesson_4.Task_3;
+
+import java.util.Scanner;
+
+public class Week {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number 1-7: ");
+        int number = input.nextInt();
+        if (number == 1){
+            System.out.println("Monday");
+        }
+        else if (number == 2){
+            System.out.println("Tuesday");
+        }
+        else if (number == 3){
+            System.out.println("Wednesday");
+        }
+        else if (number == 4){
+            System.out.println("Thursday");
+        }
+        else if (number == 5){
+            System.out.println("Friday");
+        }
+        else if (number == 6){
+            System.out.println("Saturday");
+        }
+        else if (number == 7){
+            System.out.println("Sunday");
+        }
+        else{
+            System.out.println("Wrong number !");
+        }
+    }
+
+}
