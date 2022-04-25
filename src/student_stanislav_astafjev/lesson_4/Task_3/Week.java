9893399c7894fbc6fff2c7f2f1fe594eef73a4e4
astafjev <Astafjev94@mail.ru>
2022-04-25 12:14:49 +0300

    homework

--- src/student_stanislav_astafjev/lesson_4/Task_3/Week.java
+++ src/student_stanislav_astafjev/lesson_4/Task_3/Week.java
@@ -10,22 +10,22 @@ public class Week {
         if (number == 1){
             System.out.println("Monday");
         }
-        else if (number == 2){
+         if (number == 2){
             System.out.println("Tuesday");
         }
-        else if (number == 3){
+         if (number == 3){
             System.out.println("Wednesday");
         }
-        else if (number == 4){
+         if (number == 4){
             System.out.println("Thursday");
         }
-        else if (number == 5){
+         if (number == 5){
             System.out.println("Friday");
         }
-        else if (number == 6){
+         if (number == 6){
             System.out.println("Saturday");
         }
-        else if (number == 7){
+         if (number == 7){
             System.out.println("Sunday");
         }
         else{
