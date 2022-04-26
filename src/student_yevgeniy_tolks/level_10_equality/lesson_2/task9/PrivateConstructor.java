b729fabb71930167560982b193295fafb910f876
ytolks <yevgeniy.tolks@gmail.com>
2022-04-26 16:00:07 +0300

    lesson10.level2.task9 commit

---
+++ src/student_yevgeniy_tolks/level_10_equality/lesson_2/task9/PrivateConstructor.java
@@ -0,0 +1,6 @@
+package student_yevgeniy_tolks.level_10_equality.lesson_2.task9;
+
+class PrivateConstructor {
+    private PrivateConstructor() {
+    }
+}
---
+++ src/student_yevgeniy_tolks/level_10_equality/lesson_2/task9/PrivateConstructorDemo.java
@@ -0,0 +1,9 @@
+package student_yevgeniy_tolks.level_10_equality.lesson_2.task9;
+
+class PrivateConstructorDemo {
+    public static void main(String[] args) {
+        //PrivateConstructor constructor = new PrivateConstructor();
+        //constructor of created instance has a private access, compiler error appears
+    }
+
+}
