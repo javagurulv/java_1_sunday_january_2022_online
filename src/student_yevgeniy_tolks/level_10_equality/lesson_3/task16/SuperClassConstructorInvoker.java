d85fa132b13eb6b842b5e9084a81918e1bebfafe
ytolks <yevgeniy.tolks@gmail.com>
2022-04-26 20:15:07 +0300

    lesson10.level3.task16 commit

---
+++ src/student_yevgeniy_tolks/level_10_equality/lesson_3/task16/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package student_yevgeniy_tolks.level_10_equality.lesson_3.task16;
+
+class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
