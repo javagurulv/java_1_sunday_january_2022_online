82a90477dca2146b284ea0cca5d9369ae2dcd0a1
ytolks <yevgeniy.tolks@gmail.com>
2022-04-26 13:48:14 +0300

    lesson10.level1.task1_4 commit

--- src/student_yevgeniy_tolks/lesson_9/level_6/task30/VarArgsExampleDemo.java
+++ src/student_yevgeniy_tolks/lesson_9/level_6/task30/VarArgsExampleDemo.java
@@ -5,8 +5,5 @@ class VarArgsExampleDemo {
         VarArgsExample varArgsExample = new VarArgsExample();
         int sumOfNumber = varArgsExample.sumNumbers(1,2,3,4,5,6,7,8,9,0);
         System.out.println(sumOfNumber);
-
     }
-
-
 }
---
+++ src/student_yevgeniy_tolks/level_10_equality/lesson_1/task1/Airplane.java
@@ -0,0 +1,4 @@
+package student_yevgeniy_tolks.level_10_equality.lesson_1.task1;
+
+public class Airplane {
+}
