6ab48765a2a6daaa59f8bc9930861f51b1438b60
astafjev <Astafjev94@mail.ru>
2022-04-30 19:30:43 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_4/Task_17/Stock.java
@@ -0,0 +1,9 @@
+package student_stanislav_astafjev.lesson_4.Task_17;
+
+class Stock {
+    String company;
+    int currentPrice;
+    int minPrice;
+    int maxPrice;
+
+}
---
+++ src/student_stanislav_astafjev/lesson_4/Task_17/StockTest.java
@@ -0,0 +1,4 @@
+package student_stanislav_astafjev.lesson_4.Task_17;
+
+public class StockTest {
+}
