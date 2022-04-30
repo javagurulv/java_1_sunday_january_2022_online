fb02aa14bb8103079e32d3f4b313159a4d9a0e25
ytolks <yevgeniy.tolks@gmail.com>
2022-04-30 10:49:09 +0300

    lesson9.level7.task31 draft commit

---
+++ src/student_yevgeniy_tolks/lesson_9/level_7/task31/Book.java
@@ -0,0 +1,39 @@
+package student_yevgeniy_tolks.lesson_9.level_7.task31;
+
+import java.util.List;
+
+class Book {
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void takeABookFromLibrary(List<Book> bookList, Book book) {
+        if (isBookFoundInLibrary()) {
+            bookList.remove(book);
+        }
+    }
+
+    public boolean isBookFoundInLibrary() {
+        return author.equals(getAuthor()) && title.equals(getTitle());
+    }
+}
