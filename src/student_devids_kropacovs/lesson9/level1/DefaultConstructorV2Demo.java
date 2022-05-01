4842f495dbd2ef16949b408860a9c7245777119d
Devid1322 <devidannn132@gmail.com>
2022-05-01 18:34:17 +0200

    Lesson9 level 1

--- src/student_devids_kropacovs/lesson9/level1/DefaultConstructorV2Demo.java
+++ src/student_devids_kropacovs/lesson9/level1/DefaultConstructorV2Demo.java
@@ -1,4 +1,7 @@
 package student_devids_kropacovs.lesson9.level1;
 
 public class DefaultConstructorV2Demo {
+    public static void main(String[] args) {
+        DefaultConstructorV2 defaultConstructorV2 = new DefaultConstructorV2();
+    }
 }
