c96a38d51626a76b37a4cdddf6f146dee05e38b7
Devid1322 <devidannn132@gmail.com>
2022-05-01 18:44:50 +0200

    Lesson9 level 1 done

--- src/student_devids_kropacovs/lesson9/level1/DefaultConstructorV3.java
+++ src/student_devids_kropacovs/lesson9/level1/DefaultConstructorV3.java
@@ -1,4 +1,28 @@
 package student_devids_kropacovs.lesson9.level1;
 
 public class DefaultConstructorV3 {
+
+    private String fullName;
+    private int age;
+    private boolean male;
+    private Boolean female;
+
+    public DefaultConstructorV3(){
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean getIsMale() {
+        return male;
+    }
+
+    public Boolean getIsFemale() {
+        return female;
+    }
 }
--- src/student_devids_kropacovs/lesson9/level1/DefaultConstructorV3Demo.java
+++ src/student_devids_kropacovs/lesson9/level1/DefaultConstructorV3Demo.java
@@ -1,4 +1,13 @@
 package student_devids_kropacovs.lesson9.level1;
 
 public class DefaultConstructorV3Demo {
+
+    public static void main(String[] args) {
+        DefaultConstructorV3 defaultConstructorV3 = new DefaultConstructorV3();
+      
+        System.out.println("Default result for Get Full Method = " + defaultConstructorV3.getFullName());
+        System.out.println("Default result for Get Age Method = " + defaultConstructorV3.getAge());
+        System.out.println("Default result for Get Is Male Method = " + defaultConstructorV3.getIsMale());
+        System.out.println("Default result for Get Is Female Method = " + defaultConstructorV3.getIsFemale());
+    }
 }
