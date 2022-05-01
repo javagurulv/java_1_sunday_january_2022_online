e04114cad9b00b1e37d833d0f71b7600fd77c2b4
Devid1322 <devidannn132@gmail.com>
2022-05-01 18:32:57 +0200

    Lesson9 started

--- src/student_devids_kropacovs/lesson9/level1/DefaultConstructor.java
+++ src/student_devids_kropacovs/lesson9/level1/DefaultConstructor.java
@@ -1,4 +1,7 @@
 package student_devids_kropacovs.lesson9.level1;
 
 public class DefaultConstructor {
+
+    public DefaultConstructor() {
+    }
 }
--- src/student_devids_kropacovs/lesson9/level1/DefaultConstructorDemo.java
+++ src/student_devids_kropacovs/lesson9/level1/DefaultConstructorDemo.java
@@ -1,4 +1,9 @@
 package student_devids_kropacovs.lesson9.level1;
 
 public class DefaultConstructorDemo {
+
+    public static void main(String[] args) {
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+
+    }
 }
