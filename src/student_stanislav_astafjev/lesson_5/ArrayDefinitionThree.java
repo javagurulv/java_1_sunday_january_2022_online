8fff50dee4957ae9215c5a30afb7edf51e23f88b
astafjev <Astafjev94@mail.ru>
2022-05-02 22:24:13 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_5/ArrayDefinitionThree.java
@@ -0,0 +1,7 @@
+package student_stanislav_astafjev.lesson_5;
+
+public class ArrayDefinitionThree {
+    public static void main(String[] args) {
+        Boolean[] numbers = new Boolean[10];
+    }
+}
