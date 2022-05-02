79aedfb7d470b108143c77399492a262a809b1be
astafjev <Astafjev94@mail.ru>
2022-05-02 18:56:55 +0300

    homework

--- src/student_stanislav_astafjev/lesson_5/Task_1/ArrayDefinitionOne.java
+++ src/student_stanislav_astafjev/lesson_5/Task_1/ArrayDefinitionOne.java
@@ -1,6 +1,6 @@
 package student_stanislav_astafjev.lesson_5.Task_1;
 
-public class ArrayDefinitionOne {
+class ArrayDefinitionOne {
     public static void main(String[] args) {
         int[] numbers = new int[123];
     }
---
+++ src/student_stanislav_astafjev/lesson_5/Task_2/ArrayDefinitionTwo.java
@@ -0,0 +1,8 @@
+package student_stanislav_astafjev.lesson_5.Task_2;
+
+
+class ArrayDefinitionTwo {
+    public static void main(String[] args) {
+        byte[] numbers = new byte[10];
+    }
+}
