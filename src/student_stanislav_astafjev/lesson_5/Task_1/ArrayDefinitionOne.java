3f37668f7d26117530d8f79f80cab162df8ca8ba
astafjev <Astafjev94@mail.ru>
2022-05-02 18:53:43 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_5/Task_1/ArrayDefinitionOne.java
@@ -0,0 +1,7 @@
+package student_stanislav_astafjev.lesson_5.Task_1;
+
+public class ArrayDefinitionOne {
+    public static void main(String[] args) {
+        int[] numbers = new int[123];
+    }
+}
