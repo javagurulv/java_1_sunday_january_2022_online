14ff68d870e2fbd7a580ec84a1060d0e3e24015b
astafjev <Astafjev94@mail.ru>
2022-05-02 22:25:04 +0300

    homework

--- src/student_stanislav_astafjev/lesson_5/Task_3/ArrayDefinitionThree.java
+++ src/student_stanislav_astafjev/lesson_5/Task_3/ArrayDefinitionThree.java
@@ -1,4 +1,4 @@
-package student_stanislav_astafjev.lesson_5;
+package student_stanislav_astafjev.lesson_5.Task_3;
 
 public class ArrayDefinitionThree {
     public static void main(String[] args) {
