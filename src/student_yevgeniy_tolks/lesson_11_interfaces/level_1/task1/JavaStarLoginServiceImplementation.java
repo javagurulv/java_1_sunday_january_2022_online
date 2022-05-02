8711a6835270f09ad6a46d772374bd53497f5ed5
ytolks <yevgeniy.tolks@gmail.com>
2022-05-02 23:43:12 +0300

    lesson11.level1.task2 commit

--- src/student_yevgeniy_tolks/lesson_11_interfaces/level_1/task1/JavaStarLoginServiceImplementation.java
+++ src/student_yevgeniy_tolks/lesson_11_interfaces/level_1/task1/JavaStarLoginServiceImplementation.java
@@ -1,6 +1,8 @@
 package student_yevgeniy_tolks.lesson_11_interfaces.level_1.task1;
 
 class JavaStarLoginServiceImplementation implements JavaStarLoginService{
+
+    @Override
     public boolean canLogin(String login, String password) {
         return (login != null)
                 && login.equals("javastar")
