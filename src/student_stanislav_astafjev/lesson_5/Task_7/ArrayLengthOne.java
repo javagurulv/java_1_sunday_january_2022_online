2c9c0436aede9267650a002f13689627f58ef571
astafjev <Astafjev94@mail.ru>
2022-05-03 05:55:04 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_5/Task_7/ArrayLengthOne.java
@@ -0,0 +1,8 @@
+package student_stanislav_astafjev.lesson_5.Task_7;
+
+class ArrayLengthOne {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[-1]);
+    }
+}
