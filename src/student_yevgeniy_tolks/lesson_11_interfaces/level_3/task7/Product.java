0b68b527e2b05e23e2674c824f48b55b8f7dad8a
ytolks <yevgeniy.tolks@gmail.com>
2022-05-03 21:56:22 +0300

    lesson11.level2.task7 commit

---
+++ src/student_yevgeniy_tolks/lesson_11_interfaces/level_3/task7/Product.java
@@ -0,0 +1,14 @@
+package student_yevgeniy_tolks.lesson_11_interfaces.level_3.task7;
+
+class Product{
+
+    private String title;
+
+    public Product(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
