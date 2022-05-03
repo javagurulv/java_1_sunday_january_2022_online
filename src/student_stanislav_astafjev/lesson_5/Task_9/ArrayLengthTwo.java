c33723cd630cf7d2b421ab2dc4cfdc62d1232e99
astafjev <Astafjev94@mail.ru>
2022-05-03 17:43:44 +0300

    homework

---
+++ src/student_stanislav_astafjev/lesson_5/Task_9/ArrayLengthTwo.java
@@ -0,0 +1,8 @@
+package student_stanislav_astafjev.lesson_5.Task_9;
+
+public class ArrayLengthTwo {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+}
