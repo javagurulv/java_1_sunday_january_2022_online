a221568a95383ead47b94a69e42ee5c0c934ab01
ytolks <yevgeniy.tolks@gmail.com>
2022-05-04 23:59:16 +0300

    lesson11.level2.task9 commit

---
+++ src/student_yevgeniy_tolks/lesson_11_interfaces/level_3/task9/UtilOptionalAnswers.java
@@ -0,0 +1,20 @@
+package student_yevgeniy_tolks.lesson_11_interfaces.level_3.task9;
+
+class UtilOptionalAnswers {
+/*
+1.  Optional object is used to represent null with absent value. (null-safety)
+2.  Available or not available (true or false)
+3.
+
+ 3.1. Optional<Integer> zero = Optional.empty();
+      -> creation of empty instance
+ 3.2. Optional<Integer> oneOptional = Optional.of(one);
+      -> creation of instance using "of" method, where argument can't be null
+3.3. Optional<Integer> optional = Optional.ofNullable(getInteger());
+      -> method ofNullable is used is we expect null.
+
+4. using class methods like isPresent() etc.
+5. The get() method of Optional is simply used to return a value from the Optional object
+ */
+
+}
