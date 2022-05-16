6fb0139f32f978ab6effae16fdbb5cf103a1e0de
Devid1322 <devidannn132@gmail.com>
2022-05-16 12:19:32 +0200

    lesson 10 level 1 done

---
+++ src/student_devids_kropacovs/lesson10/level1/task6/Book.java
@@ -0,0 +1,8 @@
+package student_devids_kropacovs.lesson10.level1.task6;
+//Исправьте код класса Book так, чтобы он соответствовал принципу инкапсуляции.
+class Book {
+
+    private String title;
+    private String author;
+
+}
---
+++ src/student_devids_kropacovs/lesson10/level1/task7/PrivateClass.java
@@ -0,0 +1,9 @@
+package student_devids_kropacovs.lesson10.level1.task7;
+
+class PrivateClass {
+
+    private String title;
+    private String author;
+
+
+}
