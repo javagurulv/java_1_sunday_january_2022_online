40d5339c6627712f9e2b3082a4d1abc9091b8d8d
Devid1322 <devidannn132@gmail.com>
2022-05-16 10:51:25 +0200

    lesson 9 finished

--- src/student_devids_kropacovs/lesson9/level7/task32/task_32/Division.java
+++ src/student_devids_kropacovs/lesson9/level7/task32/task_32/Division.java
@@ -1,16 +1,15 @@
 package student_devids_kropacovs.lesson9.level7.task32.task_32;
 
-class Multiplication extends TwoArgumentMathOperation{
+class Division extends TwoArgumentMathOperation{
 
-    public Multiplication(MathOperation leftArgument,
+    public Division(MathOperation leftArgument,
                           MathOperation rightArgument) {
         super(leftArgument, rightArgument);
     }
 
     @Override
     public double calculate( ) {
-        return calculateLeftSide() * calculateRightSide();
+        return calculateLeftSide() / calculateRightSide();
     }
-
 
 }
--- src/student_devids_kropacovs/lesson9/level7/task32/task_32/MAthOperationDemo.java
+++ src/student_devids_kropacovs/lesson9/level7/task32/task_32/MAthOperationDemo.java
@@ -18,6 +18,16 @@ class MAthOperationDemo {
 		// build and calculate: (10 + 20) + (50 - 20)
 		MathOperation addition1 = new Addition(addition, subtraction);
 		System.out.println("Final Addition result = " + addition1.calculate());
+
+		//(10 - 5) * (20 / 5).
+		Argument arg5 = new Argument(5);
+		Argument arg6 = new Argument(10);
+		Argument arg7 = new Argument(20);
+
+		MathOperation subtraction2 = new Subtraction(arg6, arg5);
+		MathOperation division = new Division(arg7, arg5);
+		MathOperation multiplication = new Multiplication(subtraction2, division);
+		System.out.println("Final result of (10 - 5) * (20 / 5) = " + multiplication.calculate());
 	}
 
 }
