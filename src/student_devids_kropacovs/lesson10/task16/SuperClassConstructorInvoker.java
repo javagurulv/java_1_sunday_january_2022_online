a5e35e4d2621be991af75dc026571416db6b7c0d
Devid1322 <devidannn132@gmail.com>
2022-05-16 12:57:40 +0200

    lesson 10 done

---
+++ src/student_devids_kropacovs/lesson10/task16/SuperClassConstructorInvoker.java
@@ -0,0 +1,10 @@
+package student_devids_kropacovs.lesson10.task16;
+
+    public class SuperClassConstructorInvoker {
+        public SuperClassConstructorInvoker() {
+            super();
+            System.out.println(1);
+            System.out.println(2);
+        }
+    }
+//Super() должен быть в начале конструктора
