2618c9743e5832e514dcd077a4b6625dd45679a7
Devid1322 <devidannn132@gmail.com>
2022-05-19 16:58:16 +0200

    lesson 11 task 2 done

---
+++ src/student_devids_kropacovs/lesson11/level1/task2/ArrayUtilI.java
@@ -0,0 +1,14 @@
+package student_devids_kropacovs.lesson11.level1.task2;
+
+public interface ArrayUtilI {
+
+    int[] createArray(int arrayLength);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+
+    void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+}
