d82dd66e506d993f491d64bd424039298005545f
ytolks <yevgeniy.tolks@gmail.com>
2022-05-20 23:05:32 +0300

    lesson13.level3.task 20 commit

---
+++ src/student_yevgeniy_tolks/lesson_13_exceptions/level_3/task20/MultiCatchExample.java
@@ -0,0 +1,14 @@
+package student_yevgeniy_tolks.lesson_13_exceptions.level_3.task20;
+
+class MultiCatchExample {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+//
+//        } catch (ArithmeticException a) {   // this exception already caught, should be deleted,
+//                                            // because this class extends RuntimeException
+//                                            // RuntimeException extends Exception
+        }
+    }
+}
