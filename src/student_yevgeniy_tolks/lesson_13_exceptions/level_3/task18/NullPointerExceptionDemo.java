78ff1e9136af056724fe68a9f3309c9dc6f4f743
ytolks <yevgeniy.tolks@gmail.com>
2022-05-20 20:00:28 +0300

    lesson13.level3.task_18 commit

---
+++ src/student_yevgeniy_tolks/lesson_13_exceptions/level_3/task18/NullPointerExceptionDemo.java
@@ -0,0 +1,10 @@
+package student_yevgeniy_tolks.lesson_13_exceptions.level_3.task18;
+
+class NullPointerExceptionDemo {
+    public static void main(String[] args) {
+    Object object=null; //reference obj equals to null, but is not pointing to any object in heap
+    object.hashCode();  //by calling built-in hashcode method, where is no code to in the location
+                        //where reference is pointing, so result is obvious by NullPointerException
+                        // extended from unchecked exception class RuntimeException
+    }
+}
