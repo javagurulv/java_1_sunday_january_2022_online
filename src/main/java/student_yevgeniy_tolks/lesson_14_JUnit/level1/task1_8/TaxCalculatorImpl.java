a4dc8bfd2450b2931048cbd2864aae0024a91251
ytolks <yevgeniy.tolks@gmail.com>
2022-05-25 18:51:33 +0300

    Lesson14.level1.task1_8 commit

--- src/main/java/student_yevgeniy_tolks/lesson_14_JUnit/level1/task1_8/TaxCalculatorImpl.java
+++ src/main/java/student_yevgeniy_tolks/lesson_14_JUnit/level1/task1_8/TaxCalculatorImpl.java
@@ -1,13 +1,18 @@
 package student_yevgeniy_tolks.lesson_14_JUnit.level1.task1_8;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 class TaxCalculatorImpl implements TaxCalculator {
+
     @Override
     public BigDecimal calculate(BigDecimal income) {
-        BigDecimal taxPercentage = new BigDecimal("0.25");
-        return taxPercentage.multiply(income.setScale(0, RoundingMode.DOWN));
+        if (income.compareTo(new BigDecimal(20000)) > 0) {
+            BigDecimal incomeDeltaWhenAbove20k = income.subtract(new BigDecimal(20000));
+            BigDecimal taxToPayAmountAbove20k = incomeDeltaWhenAbove20k.multiply(new BigDecimal("0.40"));
+            BigDecimal taxToPayAmountBelow20k = new BigDecimal(20000).multiply(new BigDecimal("0.25"));
+            return taxToPayAmountBelow20k.add(taxToPayAmountAbove20k);
+        } else {
+            return income.multiply(new BigDecimal("0.25"));
+        }
     }
-
 }
--- src/test/java/student_yevgeniy_tolks/lesson_14_JUnit/level1/task1_8/TaxCalculatorImplTest.java
+++ src/test/java/student_yevgeniy_tolks/lesson_14_JUnit/level1/task1_8/TaxCalculatorImplTest.java
@@ -7,23 +7,40 @@ import java.math.BigDecimal;
 import static org.junit.Assert.*;
 
 public class TaxCalculatorImplTest {
-private TaxCalculator taxCalculator = new TaxCalculatorImpl();
+    private TaxCalculator taxCalculator = new TaxCalculatorImpl();
+
     @Test
     public void taxPaymentWhenIncomeIsZeroTest() {
         BigDecimal income = BigDecimal.ZERO;
         BigDecimal expectedTaxToPay = taxCalculator.calculate(income);
         assertEquals(expectedTaxToPay, new BigDecimal("0.00"));
     }
+
     @Test
-    public void taxPaymentWhenIncomeEquals20k(){
+    public void taxPaymentWhenIncomeEquals20k() {
         BigDecimal income = new BigDecimal(20000);
         BigDecimal expectedTaxToPay = taxCalculator.calculate(income);
-        assertEquals(expectedTaxToPay,new BigDecimal("5000.00"));
+        assertEquals(expectedTaxToPay, new BigDecimal("5000.00"));
     }
+
     @Test
-    public void taxPaymentWhenIncomeIsLessThan20k(){
-    BigDecimal income = new BigDecimal(5000);
-    BigDecimal expectedTaxToPay = taxCalculator.calculate(income);
-    assertEquals(expectedTaxToPay,new BigDecimal("1250.00"));
+    public void taxPaymentWhenIncomeIsLessThan20k() {
+        BigDecimal income = new BigDecimal(5000);
+        BigDecimal expectedTaxToPay = taxCalculator.calculate(income);
+        assertEquals(expectedTaxToPay, new BigDecimal("1250.00"));
+    }
+
+    @Test
+    public void taxPaymentWhenIncomeEquals30k() {
+        BigDecimal income = new BigDecimal(30000);
+        BigDecimal expectedTaxToPay = taxCalculator.calculate(income);
+        assertEquals(expectedTaxToPay, new BigDecimal("9000.00"));
+    }
+
+    @Test
+    public void taxPaymentWhenIncomeTaxLessThan30k(){
+        BigDecimal income = new BigDecimal(24000);
+        BigDecimal expectedTaxToPay  = taxCalculator.calculate(income);
+        assertEquals(expectedTaxToPay,new BigDecimal("6600.00"));
     }
 }
