d9032064f4e32ba1bbd4dfc8e13d126a229f388d
Devid1322 <devidannn132@gmail.com>
2022-05-26 17:43:06 +0200

    lesson 12 task 2 done

---
+++ src/main/java/student_devids_kropacovs/lesson12/level2/Task2.java
@@ -0,0 +1,15 @@
+package student_devids_kropacovs.lesson12.level2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(3);
+        Integer integer = 3;
+        integerList.add(integer);
+        //It may look like we can add both objects and primitive data types, in the first example, we do not add simple integer,
+        // instead java transform this number 3 to an object
+    }
+}
