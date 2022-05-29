43e1388608236b87685550832c3cc073969629b3
astafjev <Astafjev94@mail.ru>
2022-05-29 10:20:16 +0300

    homework

---
+++ src/main/java/student_stanislav_astafjev/lesson_14/lessonwork/TaxCalculatorImplTest.java
@@ -0,0 +1,40 @@
+package student_stanislav_astafjev.lesson_14.lessonwork;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TaxCalculatorImplTest {
+
+    // TaxCalculatorImplTest test = new TaxCalculatorImplTest()
+    // test.init() //@before
+    // test.shouldReturn25ProcTaxBefore20k()   // @test
+    // test.init() // @before
+    // test.shouldReturnZero()     // @test
+
+    private TaxCalculator calculator;
+
+    @Before
+    public void init() {
+        calculator = new TaxCalculatorImpl();
+
+    }
+    @Test //(expected = RuntimeException.class)
+    public void shouldReturn25ProcTaxBefore20k() {
+        double realTax = calculator.calculate(10000.0);
+        assertEquals(realTax,2500.0,0.0001);
+    }
+    @Test
+    public void shouldReturnZero(){
+        double realTax = calculator.calculate(0.0);
+        assertEquals(realTax,0.0,0.0001);
+    }
+    @Ignore
+    public void shouldIgnore() {
+        double realTax = calculator.calculate(0.0);
+        assertEquals(realTax,0.0,0.0001);
+
+    }
+}
---
+++ src/main/java/student_stanislav_astafjev/lesson_5/Task_13/ArrayOfIntegers.java
@@ -0,0 +1,8 @@
+package student_stanislav_astafjev.lesson_5.Task_13;
+
+public class ArrayOfIntegers {
+    public static void main(String[] args) {
+
+
+    }
+}
