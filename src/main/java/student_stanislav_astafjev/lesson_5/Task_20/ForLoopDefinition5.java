3beb6928e8b681c196edf3d4d0ccc0a267fefcdd
astafjev <Astafjev94@mail.ru>
2022-05-31 10:05:09 +0300

    homework

---
+++ src/main/java/student_stanislav_astafjev/lesson_5/Task_20/ForLoopDefinition5.java
@@ -0,0 +1,10 @@
+package student_stanislav_astafjev.lesson_5.Task_20;
+
+class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
