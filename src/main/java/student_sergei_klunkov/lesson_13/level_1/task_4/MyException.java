bec78f0f9f02b11b93d7ddcde6f055dce8cc0ce7
SergioTheMaster <sergei.klunkov@gmail.com>
2022-06-06 14:20:42 +0300

    13 Lesson!

--- src/main/java/student_sergei_klunkov/lesson_13/level_1/task_4/MyException.java
+++
@@ -1,9 +0,0 @@
-package student_sergei_klunkov.lesson_13.level_1.task_4;
-
-class MyException extends Exception {
-
-    public MyException(String message) {
-
-    }
-
-}
---
+++ src/main/java/student_sergei_klunkov/lesson_13/level_1/task_4_5/MyException.java
@@ -0,0 +1,11 @@
+package student_sergei_klunkov.lesson_13.level_1.task_4_5;
+
+class MyException extends Exception {
+
+    private String message;
+    public MyException(String message) {
+        this.message = message;
+
+    }
+
+}
