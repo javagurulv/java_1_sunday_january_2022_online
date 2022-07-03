a03329ee38e10c2e0876e02ed2302b4ff93d7962
DD-53 <deniss0053@gmail.com>
2022-07-03 21:13:23 +0200

    Lesson 9. Homework. Level 2. Task 8: Computer Player.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_9_homework/level_2_intern/task_8/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package student_deniss_dubko.deniss_dubko_lesson_9_homework.level_2_intern.task_8;
+
+class ComputerPlayer extends Player {
+
+    public ComputerPlayer(String name) {
+        super(name);
+    }
+
+}
