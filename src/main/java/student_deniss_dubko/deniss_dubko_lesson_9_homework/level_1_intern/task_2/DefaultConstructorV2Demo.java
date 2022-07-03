8e1bbe5b35695680236aa4cb2686c66341aab4da
DD-53 <deniss0053@gmail.com>
2022-07-03 20:13:40 +0200

    Lesson 9. Homework. Level 1. Default constructor V2.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_9_homework/level_1_intern/task_2/DefaultConstructorV2Demo.java
@@ -0,0 +1,11 @@
+package student_deniss_dubko.deniss_dubko_lesson_9_homework.level_1_intern.task_2;
+
+class DefaultConstructorV2Demo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructorV2 constructor = new DefaultConstructorV2();
+        System.out.println(constructor.testText);
+    }
+
+}
