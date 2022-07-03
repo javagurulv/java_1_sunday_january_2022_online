aa2bc9b12724ef759dab9288a06ed5d3e62d4c2d
DD-53 <deniss0053@gmail.com>
2022-07-03 21:35:01 +0200

    Lesson 9. Homework. Level 3. Task 11: Loan System.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_9_homework/level_3_junior/task_11/Human.java
@@ -0,0 +1,6 @@
+package student_deniss_dubko.deniss_dubko_lesson_9_homework.level_3_junior.task_11;
+
+class Human {
+
+
+}
---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_9_homework/level_3_junior/task_11/LoanSystem.java
@@ -0,0 +1,7 @@
+package student_deniss_dubko.deniss_dubko_lesson_9_homework.level_3_junior.task_11;
+
+abstract class LoanSystem {
+
+    abstract boolean canReceiveLoan(Human human);
+
+}
