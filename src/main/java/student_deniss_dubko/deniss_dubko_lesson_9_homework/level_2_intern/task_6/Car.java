d0633039d74ef2436327f6144d115e343d9c052f
DD-53 <deniss0053@gmail.com>
2022-07-03 20:58:43 +0200

    Lesson 9. Homework. Level 2. Task 6: Porsche 911.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_9_homework/level_2_intern/task_6/Car.java
@@ -0,0 +1,11 @@
+package student_deniss_dubko.deniss_dubko_lesson_9_homework.level_2_intern.task_6;
+
+class Car {
+
+    private int releaseYear;
+
+    public Car(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+}
