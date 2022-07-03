12d105795d951d8d00612d802bc6c0777665363f
DD-53 <deniss0053@gmail.com>
2022-07-04 01:07:48 +0200

    Lesson 9. Homework. Level 3. Task 13: Abstract Class Car.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_9_homework/level_3_junior/task_13/ElectricCar.java
@@ -0,0 +1,16 @@
+package student_deniss_dubko.deniss_dubko_lesson_9_homework.level_3_junior.task_13;
+
+class ElectricCar extends Car {
+
+    private int batteryCharge;
+
+    public ElectricCar(int batteryCharge) {
+        this.batteryCharge = batteryCharge;
+    }
+
+    @Override
+    void accelerate() {
+
+    }
+
+}
