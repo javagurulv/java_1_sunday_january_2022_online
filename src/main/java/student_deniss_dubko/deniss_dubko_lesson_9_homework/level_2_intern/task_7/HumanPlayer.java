1dcdb5f453eee68ab6391df1f1269b76d544ee5f
DD-53 <deniss0053@gmail.com>
2022-07-03 21:05:30 +0200

    Lesson 9. Homework. Level 2. Task 7: Human Player.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_9_homework/level_2_intern/task_7/HumanPlayer.java
@@ -0,0 +1,10 @@
+package student_deniss_dubko.deniss_dubko_lesson_9_homework.level_2_intern.task_7;
+
+class HumanPlayer extends Player {
+
+    HumanPlayer(String name) {
+
+        super(name);
+    }
+
+}
