29ca8ca360c230793e09a9daf0ecbf61ca6cb6bc
DD-53 <deniss0053@gmail.com>
2022-07-04 21:41:31 +0200

    Lesson 10. Homework. Level 1. Task 7: Private Class.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_10_homework/level_1_intern/task_7/PrivateClass.java
@@ -0,0 +1,8 @@
+package student_deniss_dubko.deniss_dubko_lesson_10_homework.level_1_intern.task_7;
+
+class PrivateClass { // Impossible use "private" on Class level
+
+    private String title;
+    private String author;
+
+}
