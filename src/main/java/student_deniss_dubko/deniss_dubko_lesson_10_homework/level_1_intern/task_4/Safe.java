42798f943eeb23c5ba95617d76d54752b0e1fdf6
DD-53 <deniss0053@gmail.com>
2022-07-04 21:32:37 +0200

    Lesson 10. Homework. Level 1. Task 4: Safe.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_10_homework/level_1_intern/task_4/Safe.java
@@ -0,0 +1,15 @@
+package student_deniss_dubko.deniss_dubko_lesson_10_homework.level_1_intern.task_4;
+
+class Safe {
+
+    public int password;
+    public int moneyAmount;
+
+    public Safe(int password,
+                int moneyAmount) {
+
+        this.password = password;
+        this.moneyAmount = moneyAmount;
+    }
+
+}
