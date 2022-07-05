7ffe8a35602ec9379ed099dc4dc69044b7b3a593
DD-53 <deniss0053@gmail.com>
2022-07-05 21:08:36 +0200

    Lesson 12. Homework. Level 1. Task 5: Books List.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_12_homework/level_1_intern/task_5_book/Book.java
@@ -0,0 +1,36 @@
+package student_deniss_dubko.deniss_dubko_lesson_12_homework.level_1_intern.task_5_book;
+
+import java.util.Objects;
+
+class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public String toString() {
+        return title + author;
+    }
+
+}
