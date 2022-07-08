1e1fbd6855ab14788c221a0ab583f42ca6b7b9de
DD-53 <deniss0053@gmail.com>
2022-07-09 00:09:51 +0200

    Lesson 11. Homework. Level 1. Task 2: Array Util.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_11_homework/level_1_intern/task_2/ArrayUtil.java
@@ -0,0 +1,15 @@
+package student_deniss_dubko.deniss_dubko_lesson_11_homework.level_1_intern.task_2;
+
+interface ArrayUtil {
+
+    int[] createArray(int arrayLength);
+
+    void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+
+}
