ee5306f5edcb008980003134e6a086d5550d42b5
DD-53 <deniss0053@gmail.com>
2022-07-08 22:40:18 +0200

    Lesson 10. Homework. Level 2. Task 13: ClassX and Class Y.

---
+++ src/main/java/student_deniss_dubko/deniss_dubko_lesson_10_homework/level_2_intern/task_13/ClassY.java
@@ -0,0 +1,10 @@
+package student_deniss_dubko.deniss_dubko_lesson_10_homework.level_2_intern.task_13;
+
+class ClassY extends ClassX {
+
+    public ClassY(int i) {
+        super(i);
+        System.out.println(2);
+    }
+
+}
