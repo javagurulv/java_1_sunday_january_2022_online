9ed969b44edc679c6d2c24d0e11795a7d0adcc49
DD-53 <deniss0053@gmail.com>
2023-07-10 23:07:57 +0200

    Practice.

---
+++ src/main/java/student_deniss_dubko/practice/lesson_3_oop/level_1/task_1/Robot.java
@@ -0,0 +1,16 @@
+package student_deniss_dubko.practice.lesson_3_oop.level_1.task_1;
+
+class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+
+    public Robot() {
+
+    }
+}
